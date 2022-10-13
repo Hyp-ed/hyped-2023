a205b5273ccd6c05d44ab59f7d083e26f66087b4
Knhwong <47486931+Knhwong@users.noreply.github.com>
2022-10-13 19:08:36 +0100

    Preliminary Code
    Basic code copypasting

--- lib/io/gpio.cpp
+++ lib/io/gpio.cpp
@@ -1,10 +1,46 @@
 #include "gpio.hpp"
 
+#include <fcntl.h>
+#include <utils/system.hpp>
+
 namespace hyped::io {
 
 Gpio::Gpio()
 {
   // TODO: implement
+  if (!initialised_) initialise();
+}
+
+void Gpio::initialise()
+{
+  int fd;  // file descriptor
+  off_t offset;
+  void *base;
+
+  fd = open("/dev/mem", O_RDWR);
+  if (fd < 0) {
+    kLog.error("could not open /dev/mem");
+    return;
+  }
+
+  for (size_t i = 0; i < Gpio::kBankNum; ++i) {
+    offset = Gpio::kBases[i];
+    /**
+     * @brief mmap() creates a new mapping in the virtual address space of the calling process
+        The starting address is zero
+        The length argument is kMmapSize
+     * void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+     */
+    base = mmap(0, Gpio::kMmapSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
+    if (base == MAP_FAILED) {
+      kLog.error("could not map bank %d", offset);
+      return;
+    }
+
+    base_mapping_[i] = base;
+  }
+  atexit(uninitialise);
+  initialised_ = true;
 }
 
 std::optional<GpioReader> Gpio::getReader(const uint8_t pin)
--- lib/io/gpio.hpp
+++ lib/io/gpio.hpp
@@ -3,17 +3,45 @@
 #include "gpio_reader.hpp"
 #include "gpio_writer.hpp"
 
+#include <fcntl.h>
+
 #include <cstdint>
 #include <optional>
 
 namespace hyped::io {
 
+//Not dealing with log right now
 class Gpio {
  public:
-  Gpio();
+    Gpio();
 
-  std::optional<GpioReader> getReader(const std::uint8_t pin);
-  std::optional<GpioWriter> getWriter(const std::uint8_t pin);
+    std::optional<GpioReader> getReader(const std::uint8_t pin);
+    std::optional<GpioWriter> getWriter(const std::uint8_t pin);
+  private:
+
+    static constexpr std::array<off_t, kBankNum> kBases = {0x44e07000, 0x4804c000, 0x481ac000, 0x481ae000};
+    static constexpr uint32_t kMmapSize = 0x1000;
+    static constexpr uint32_t kData     = 0x138;
+    static constexpr uint32_t kClear    = 0x190;
+    static constexpr uint32_t kSet      = 0x194;
+    Gpio()                              = delete;
+
+    static void initialise();
+    static void uninitialise();
+
+    static void *base_mapping_[kBankNum];
+    void exportGPIO();
+    void attach();
+    void setupWait();
+
+    uint32_t pin_;
+    Direction direction_;  
+
+    volatile uint32_t *set_;    // set register
+    volatile uint32_t *clear_;  // clear register
+    volatile uint32_t *data_;   // data register
+    uint32_t pin_mask_;         // mask for register access to this pin
+    int fd_;                    // file pointer to /sys/class/gpio/gpioXX/value
 };
 
 }  // namespace hyped::io
