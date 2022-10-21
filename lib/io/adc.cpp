407cc4076186ae8824a25be43f5383fb45de72b8
SnickeyX <s2152674@ed.ac.uk>
2022-10-21 03:24:20 +0100

    adc changes

--- lib/io/adc.cpp
+++ lib/io/adc.cpp
@@ -5,29 +5,42 @@
 
 namespace hyped::io {
     
-uint16_t resetAndRead4(int file_descriptor)
-{
-  char buf[4];                              // buffer size 4 for fs value
-  lseek(file_descriptor, 0, SEEK_SET);      // reset file pointer
-  read(file_descriptor, buf, sizeof(buf));  // actually consume new data, changes value in buffer
-  return std::atoi(buf);
-}
-
 Adc::Adc(const uint32_t pin) : pin_(pin)
-{
-}
-
-uint16_t Adc::read()
 {
   char buf[100];
   snprintf(buf, sizeof(buf), "/sys/bus/iio/devices/iio:device0/in_voltage%i_raw", pin_);
   file_ = open(buf, O_RDONLY);
   if (file_ < 0) { /* TODO: log that there is a problem reading raw voltage for pin_*/ ;}
   /* TODO: log the file descriptor */
-  const uint16_t val = resetAndRead4(file_);
-  /* TODO: log the value */ 
-  close(file_);
-  return val;
+}
+
+Adc::~Adc()
+{
+  close(file_); 
+}
+
+std::optional<uint16_t> Adc::read()
+{
+  const std::optional<uint16_t> val = resetAndRead4(file_);
+  if(val) 
+  {
+      /* TODO: log the value */ 
+      return *val;
+  }
+  return std::nullopt; 
+}
+
+std::optional<uint16_t> resetAndRead4(int file_descriptor)
+{
+  char buf[4];                                                 // buffer size 4 for fs value
+  lseek(file_descriptor, 0, SEEK_SET);                         // reset file pointer
+  int bytes_read = read(file_descriptor, &buf, sizeof(buf));   // actually consume new data, changes value in buffer
+  if(bytes_read != sizeof(buf)) 
+  {
+    /*TODO: log that we failed in reading 4 bytes or reached EOF*/
+    return std::nullopt;                                       // returning NULL since we did not get any value
+  }
+  return std::atoi(buf);
 }
 
 }  // namespace hyped::io
