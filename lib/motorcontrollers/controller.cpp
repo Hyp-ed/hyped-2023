b42a0c3e9e5c1a13cd023d6668839258d6348f6e
tom <tfeholland@gmail.com>
2022-10-24 18:59:33 +0100

    Commenting out function to be rewritten

--- lib/motorcontrollers/controller.cpp
+++ lib/motorcontrollers/controller.cpp
@@ -65,42 +65,7 @@ void processEmergencyMessage(core::types::Canframe data) {
   }
 
 void processSdoMessage(core::types::Canframe data) {
-    switch(data) {
-        case 1:
-            log_.error("Controller temperature exceeded");
-            break;
-        case 2:
-            log_.error("Motor temperature exceeded");
-            break;
-        case 4:
-            log_.error("DC link under voltage");
-            break;
-        case 8:
-            log_.error("DC link over voltage");
-            break;
-        case 16:
-            log_.error("DC link over current");
-            break;
-        case 32:
-            log_.error("Stall protection active");
-            break;
-        case 64:
-            log_.error("Max velocity exceeded");
-            break;
-        case 128:
-            log_.error("BMS proposed power");
-            break;
-        case 256:
-            log_.error("Capacitor temperature exceeded");
-            break;
-        case 512:
-            log_.error("I2T protection");
-            break;
-        case 1024:
-            log_.error("Field weakening active");
-            break;
-        default:
-            log_.error("Unknown Error");
+    // TODO : Implement
 
     }
 
