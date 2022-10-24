e262ca3c60f51e2de82c16d81cd44c820825af80
SnickeyX <s2152674@ed.ac.uk>
2022-10-24 17:27:38 +0100

    requested changes - the comeback

--- lib/io/adc.cpp
+++ lib/io/adc.cpp
@@ -3,49 +3,47 @@
 #include <fcntl.h>   // define O_WONLY and O_RDONLY
 #include <unistd.h>  // close()
 
-namespace hyped::io { 
+namespace hyped::io {
 
 Adc::Adc(const uint32_t pin) : pin_(pin)
 {
   char buf[100];
   snprintf(buf, sizeof(buf), "/sys/bus/iio/devices/iio:device0/in_voltage%i_raw", pin_);
   file_ = open(buf, O_RDONLY);
-  if (file_ < 0) { /* TODO: log that there is a problem reading raw voltage for pin_*/ ;}
+  if (file_ < 0) { /* TODO: log that there is a problem reading raw voltage for pin_*/; }
 }
 
 Adc::~Adc()
 {
-  close(file_); 
+  close(file_);
 }
 
 std::optional<uint16_t> Adc::read()
 {
   const std::optional<uint16_t> raw_voltage = resetAndRead4(file_);
-  if(raw_voltage) 
-  {
-      /* TODO: log the value */ 
-      return *raw_voltage;
+  if (raw_voltage) {
+    /* TODO: log the value */
+    return *raw_voltage;
   }
-  return std::nullopt; 
+  return std::nullopt;
 }
 
 std::optional<uint16_t> resetAndRead4(const int file_descriptor)
 {
-  char read_buffer[4];                                                               // buffer size 4 for fs value
-  const auto offset = lseek(file_descriptor, 0, SEEK_SET);                           // reset file pointer
-  if(offset != 0)
-  {
+  const auto offset = lseek(file_descriptor, 0, SEEK_SET);    // reset file pointer
+  if (offset != 0) {
     /*TODO: log that we failed in resetting file_descriptor's file offset*/
-    return std::nullopt; 
+    return std::nullopt;
   }
-  const int bytes_read = read(file_descriptor, &read_buffer, sizeof(read_buffer));   // actually consume new data, changes value in buffer
-  if(bytes_read != sizeof(read_buffer)) 
-  {
+  char read_buffer[4];                                        // buffer size 4 for fs value
+  const int num_bytes_read = read(file_descriptor, &read_buffer,
+                              sizeof(read_buffer));           // actually consume new data, changes value in buffer
+  if (num_bytes_read != sizeof(read_buffer)) {
     /*TODO: log that we failed in reading 4 bytes or reached EOF*/
-    return std::nullopt;                                                             // returning NULL since we did not get any value
+    return std::nullopt;  // returning NULL since we did not get any value
   }
-  const int raw_voltage = std::atoi(read_buffer); 
-  return static_cast<uint16_t>(raw_voltage);                                         // max value is 2^12-1 = 4095
+  const int raw_voltage = std::atoi(read_buffer);
+  return static_cast<uint16_t>(raw_voltage);  // max value is 2^12-1 = 4095
 }
 
 }  // namespace hyped::io
--- lib/io/adc.hpp
+++ lib/io/adc.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <optional>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>  // for atoi
+#include <optional>
 
 namespace hyped::io {
 
@@ -13,22 +13,21 @@ class Adc {
    * @param pin is one of the 6 analogue input pins on the bbb
    */
   Adc(const uint32_t pin);
-  ~Adc(); 
+  ~Adc();
 
   /**
    * @brief reads AIN value from file system
    *
-   * @return uint16_t return two bytes for [0,4095] range 
+   * @return uint16_t return two bytes for [0,4095] range
    *         because the BBB has 12-bit ADCs (2^12 = 4096)
    */
   std::optional<uint16_t> read();
 
-
   /**
    * @param    file_descriptor specifying the file voltage values are read from
    * @return   uint16_t returns two bytes of current voltage data
-   */ 
-  std::optional<uint16_t> resetAndRead4(const int file_descriptor); 
+   */
+  std::optional<uint16_t> resetAndRead4(const int file_descriptor);
 
  private:
   uint32_t pin_;
