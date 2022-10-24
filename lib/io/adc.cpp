41f57b7f7d2f961155ea8334baa45c245bb71291
SnickeyX <s2152674@ed.ac.uk>
2022-10-24 09:37:51 +0100

    requested changes and lseek check

--- lib/io/adc.cpp
+++ lib/io/adc.cpp
@@ -11,7 +11,6 @@ Adc::Adc(const uint32_t pin) : pin_(pin)
   snprintf(buf, sizeof(buf), "/sys/bus/iio/devices/iio:device0/in_voltage%i_raw", pin_);
   file_ = open(buf, O_RDONLY);
   if (file_ < 0) { /* TODO: log that there is a problem reading raw voltage for pin_*/ ;}
-  /* TODO: log the file descriptor */
 }
 
 Adc::~Adc()
@@ -21,26 +20,32 @@ Adc::~Adc()
 
 std::optional<uint16_t> Adc::read()
 {
-  const std::optional<uint16_t> val = resetAndRead4(file_);
-  if(val) 
+  const std::optional<uint16_t> raw_voltage = resetAndRead4(file_);
+  if(raw_voltage) 
   {
       /* TODO: log the value */ 
-      return *val;
+      return *raw_voltage;
   }
   return std::nullopt; 
 }
 
-std::optional<uint16_t> resetAndRead4(int file_descriptor)
+std::optional<uint16_t> resetAndRead4(const int file_descriptor)
 {
-  char buf[4];                                                 // buffer size 4 for fs value
-  lseek(file_descriptor, 0, SEEK_SET);                         // reset file pointer
-  int bytes_read = read(file_descriptor, &buf, sizeof(buf));   // actually consume new data, changes value in buffer
-  if(bytes_read != sizeof(buf)) 
+  char read_buffer[4];                                                               // buffer size 4 for fs value
+  const auto offset = lseek(file_descriptor, 0, SEEK_SET);                           // reset file pointer
+  if(offset != 0)
+  {
+    /*TODO: log that we failed in resetting file_descriptor's file offset*/
+    return std::nullopt; 
+  }
+  const int bytes_read = read(file_descriptor, &read_buffer, sizeof(read_buffer));   // actually consume new data, changes value in buffer
+  if(bytes_read != sizeof(read_buffer)) 
   {
     /*TODO: log that we failed in reading 4 bytes or reached EOF*/
-    return std::nullopt;                                       // returning NULL since we did not get any value
+    return std::nullopt;                                                             // returning NULL since we did not get any value
   }
-  return std::atoi(buf);
+  const int raw_voltage = std::atoi(read_buffer); 
+  return static_cast<uint16_t>(raw_voltage);                                         // max value is 2^12-1 = 4095
 }
 
 }  // namespace hyped::io
