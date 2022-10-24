23424fcdeddccf975ab6605b119fe34275807fa5
Kshitij Sharma <kshitij4sharma@gmail.com>
2022-10-24 18:19:02 +0100

    Rename to timestamp

--- lib/core/logger.cpp
+++ lib/core/logger.cpp
@@ -14,12 +14,12 @@ Logger::Logger(const char *const label, const LogLevel level, const core::ITimeS
 void Logger::printHead(FILE *file, const char *title)
 {
   const auto time_point         = time_source_.now();
-  const auto ttime_t            = std::chrono::system_clock::to_time_t(time_point);
-  const auto time_point_seconds = std::chrono::system_clock::from_time_t(ttime_t);
+  const auto timestamp            = std::chrono::system_clock::to_time_t(time_point);
+  const auto time_point_seconds = std::chrono::system_clock::from_time_t(timestamp);
   const std::chrono::milliseconds time_point_milliseconds
     = duration_cast<std::chrono::milliseconds>(time_point - time_point_seconds);
   const long long time_milliseconds = time_point_milliseconds.count();
-  const std::tm *time_struct        = localtime(&ttime_t);
+  const std::tm *time_struct        = localtime(&timestamp);
   fprintf(file, "%02d:%02d:%02d.%03lld %s[%s] ", time_struct->tm_hour, time_struct->tm_min,
           time_struct->tm_sec, time_milliseconds, title, label_);
 }
