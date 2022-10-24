052abe46a8957f52a8a90032f67a3b819a8b7449
SnickeyX <s2152674@ed.ac.uk>
2022-10-24 17:30:54 +0100

    comment alignment

--- lib/io/adc.cpp
+++ lib/io/adc.cpp
@@ -30,20 +30,20 @@ std::optional<uint16_t> Adc::read()
 
 std::optional<uint16_t> resetAndRead4(const int file_descriptor)
 {
-  const auto offset = lseek(file_descriptor, 0, SEEK_SET);    // reset file pointer
+  const auto offset = lseek(file_descriptor, 0, SEEK_SET);        // reset file pointer
   if (offset != 0) {
     /*TODO: log that we failed in resetting file_descriptor's file offset*/
     return std::nullopt;
   }
-  char read_buffer[4];                                        // buffer size 4 for fs value
-  const int num_bytes_read = read(file_descriptor, &read_buffer,
-                              sizeof(read_buffer));           // actually consume new data, changes value in buffer
+  char read_buffer[4];                                            // buffer size 4 for fs value
+  const int num_bytes_read = read(file_descriptor, &read_buffer,  
+                                  sizeof(read_buffer));           // actually consume new data, changes value in buffer
   if (num_bytes_read != sizeof(read_buffer)) {
     /*TODO: log that we failed in reading 4 bytes or reached EOF*/
-    return std::nullopt;  // returning NULL since we did not get any value
+    return std::nullopt;                                          // returning NULL since we did not get any value
   }
   const int raw_voltage = std::atoi(read_buffer);
-  return static_cast<uint16_t>(raw_voltage);  // max value is 2^12-1 = 4095
+  return static_cast<uint16_t>(raw_voltage);                      // max value is 2^12-1 = 4095
 }
 
 }  // namespace hyped::io
