9a031af89397661c678ffed0909bd0715eea1fdc
Franz Miltz <franz@miltz.me>
2022-10-28 11:55:20 +0100

    Format logger tests

--- test/core/logger.cpp
+++ test/core/logger.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 
-#include <utils/manual_time.hpp>
 #include <core/logger.hpp>
+#include <utils/manual_time.hpp>
 
 namespace hyped::test {
 
-void testStdoutLog(const core::LogLevel level, const utils::ManualTime &manual_time, const std::string expected_output)
+void testStdoutLog(const core::LogLevel level,
+                   const utils::ManualTime &manual_time,
+                   const std::string expected_output)
 {
   testing::internal::CaptureStdout();
   core::Logger logger("test", level, manual_time);
@@ -13,13 +15,15 @@ void testStdoutLog(const core::LogLevel level, const utils::ManualTime &manual_t
   ASSERT_EQ(testing::internal::GetCapturedStdout(), expected_output);
 }
 
-void testStderrLog(const core::LogLevel level, const utils::ManualTime &manual_time, const std::string expected_output)
+void testStderrLog(const core::LogLevel level,
+                   const utils::ManualTime &manual_time,
+                   const std::string expected_output)
 {
   testing::internal::CaptureStderr();
   core::Logger logger("test", level, manual_time);
   logger.log(level, "test");
   ASSERT_EQ(testing::internal::GetCapturedStderr(), expected_output);
-} 
+}
 
 TEST(Logger, Stdout)
 {
@@ -28,7 +32,6 @@ TEST(Logger, Stdout)
   testStdoutLog(core::LogLevel::kInfo, manual_time, "01:00:00.000 INFO[test] test\n");
   testStdoutLog(core::LogLevel::kFatal, manual_time, "");
 }
-
 
 TEST(Logger, Stderr)
 {
