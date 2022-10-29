588751df5ae62b9144416a2aac4f537ea8096815
SnickeyX <s2152674@ed.ac.uk>
2022-10-29 21:47:57 +0100

    i2c sensors interface

---
+++ lib/sensors/sensors.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+#include <cstdio>
+
+namespace hyped::sensors {
+
+enum class WriteResult { kFailure, kSuccess };
+
+/**
+ * If a sensor is to be used with an I2C mux, it must inherit from this abstract class.
+ */
+class II2cSensor {
+ public:
+  /*
+   * This function carries out the initilization steps for a particular sensor.
+   */
+  virtual bool configure()    = 0;
+  virtual WriteResult write() = 0;
+  virtual uint16_t read()     = 0;
+};
+
+}  // namespace hyped::sensors
