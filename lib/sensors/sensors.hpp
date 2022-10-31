434e56c44398343566ebb4c029b1bcb81d55327b
SnickeyX <s2152674@ed.ac.uk>
2022-10-31 16:29:25 +0000

    removed write

--- lib/sensors/sensors.hpp
+++ lib/sensors/sensors.hpp
@@ -6,8 +6,6 @@
 
 namespace hyped::sensors {
 
-enum class WriteResult { kFailure, kSuccess };
-
 /**
  * If a sensor is to be used with an I2C mux, it must inherit from this abstract class.
  */
@@ -17,7 +15,6 @@ class II2cSensor {
    * This function carries out the initilization steps for a particular sensor.
    */
   virtual bool configure()               = 0;
-  virtual WriteResult write()            = 0;
   virtual std::optional<uint16_t> read() = 0;
 };
 
