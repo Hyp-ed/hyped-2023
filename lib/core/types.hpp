edf918ccf952ba248d2d3c53c56c44c749b17545
Tom <tomlonergan91@gmail.com>
2022-10-31 12:27:48 +0000

    Moved CanFrame def

--- lib/core/types.hpp
+++ lib/core/types.hpp
@@ -7,18 +7,4 @@ enum class DigitalSignal { kLow = 0, kHigh };
 
 using Float = float;
 
-#if LINUX
-#include <linux/can.h>
-struct CanFrame = can_frame;
-#else
-struct CanFrame {
-  uint32_t can_id; /* 32 bit CAN_ID + EFF/RTR/ERR flags */
-  uint8_t can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
-  uint8_t __pad;   /* padding */
-  uint8_t __res0;  /* reserved / padding */
-  uint8_t __res1;  /* reserved / padding */
-  std::array<uint8_t, 8> data;
-};
-#endif
-
 }  // namespace hyped::core
--- lib/io/can.hpp
+++ lib/io/can.hpp
@@ -3,10 +3,19 @@
 #include <core/logger.hpp>
 #include <core/types.hpp>
 
-#if LINUX
+#ifndef LINUX
 #include <linux/can.h>
+struct CanFrame = can_frame;
+#else
+struct CanFrame {
+  uint32_t can_id; /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+  uint8_t can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
+  uint8_t __pad;   /* padding */
+  uint8_t __res0;  /* reserved / padding */
+  uint8_t __res1;  /* reserved / padding */
+  std::array<uint8_t, 8> data;
+};
 #endif
-
 namespace hyped::io {
 
 enum class CanResult { kFailure, kSuccess };
