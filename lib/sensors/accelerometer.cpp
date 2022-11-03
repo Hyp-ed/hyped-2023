40e4a29c54a1f18f8ab9c5b4231e96613035640d
SnickeyX <s2152674@ed.ac.uk>
2022-11-03 22:20:21 +0000

    formatting

--- lib/sensors/accelerometer.cpp
+++ lib/sensors/accelerometer.cpp
@@ -2,27 +2,26 @@
 
 namespace hyped::sensors {
 
-    Accelerometer::Accelerometer(io::I2c &i2c, core::ILogger &log)
-        : log_(log), i2c_(i2c)
-    {
-    }
+Accelerometer::Accelerometer(io::I2c &i2c, core::ILogger &log) : log_(log), i2c_(i2c)
+{
+}
 
-    Accelerometer::~Accelerometer()
-    {
-    }
+Accelerometer::~Accelerometer()
+{
+}
 
-    std::optional<float> Accelerometer::read() 
-    {
-        // TODOLater: read from i2c
-        log_.log(hyped::core::LogLevel::kFatal, "Accelerometer read not implemented");
-        return std::nullopt;
-    }
+std::optional<float> Accelerometer::read()
+{
+  // TODOLater: read from i2c
+  log_.log(hyped::core::LogLevel::kFatal, "Accelerometer read not implemented");
+  return std::nullopt;
+}
 
-    bool Accelerometer::configure()
-    {
-        // TODOLater: configure i2c
-        log_.log(hyped::core::LogLevel::kFatal, "Accelerometer configure not implemented");
-        return false; 
-    }
+bool Accelerometer::configure()
+{
+  // TODOLater: configure i2c
+  log_.log(hyped::core::LogLevel::kFatal, "Accelerometer configure not implemented");
+  return false;
+}
 
-} // namespace hyped::sensors
+}  // namespace hyped::sensors
--- lib/sensors/accelerometer.hpp
+++ lib/sensors/accelerometer.hpp
@@ -12,17 +12,16 @@
 namespace hyped::sensors {
 
 class Accelerometer : II2cSensor<float> {
-  public:
-    Accelerometer(io::I2c &i2c, hyped::core::ILogger &log);
-    ~Accelerometer();
+ public:
+  Accelerometer(io::I2c &i2c, hyped::core::ILogger &log);
+  ~Accelerometer();
 
-    bool configure(); 
-    std::optional<float> read();
+  bool configure();
+  std::optional<float> read();
 
-  private:
-    hyped::core::ILogger &log_; 
-    hyped::io::I2c &i2c_; 
-
+ private:
+  hyped::core::ILogger &log_;
+  hyped::io::I2c &i2c_;
 };
 
 }  // namespace hyped::sensors
