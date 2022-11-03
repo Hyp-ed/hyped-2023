aad66142981029e14a8ef9aa435e5c0a64f9ef1b
Kshitij Sharma <kshitij4sharma@gmail.com>
2022-11-03 19:12:11 +0000

    Fix max's weird merge artifact

--- lib/navigation/navigator.hpp
+++ lib/navigation/navigator.hpp
@@ -5,28 +5,6 @@
 namespace hyped::navigation {
 
 class Navigator {
-struct Trajectory {
-  core::Float displacement;
-  core::Float velocity;
-  core::Float acceleration;
-};
-
-inline bool operator==(const Trajectory &lhs, const Trajectory &rhs)
-{
-  return lhs.displacement == rhs.displacement && lhs.velocity == rhs.velocity
-         && lhs.acceleration == rhs.acceleration;
-}
-
-// Explicitly defined not equal to operator because gtest got sad
-inline bool operator!=(const Trajectory &lhs, const Trajectory &rhs)
-{
-  return !(lhs == rhs);
-}
-
-inline Trajectory zero_trajectory = {0, 0, 0};
-
-template<typename SensorData>
-class INavigator {
  public:
   Navigator();
 
