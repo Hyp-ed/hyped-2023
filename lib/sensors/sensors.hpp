db03dfd1ae92371e57f9e21eecf86733570945f4
SnickeyX <s2152674@ed.ac.uk>
2022-11-03 18:25:34 +0000

    templating time

--- lib/sensors/sensors.hpp
+++ lib/sensors/sensors.hpp
@@ -9,13 +9,14 @@ namespace hyped::sensors {
 /**
  * If a sensor is to be used with an I2C mux, it must inherit from this abstract class.
  */
+template <typename T>
 class II2cSensor {
  public:
   /*
    * This function carries out the initilization steps for a particular sensor.
    */
-  virtual bool configure()               = 0;
-  virtual std::optional<uint16_t> read() = 0;
+  virtual bool configure()        = 0;
+  virtual std::optional<T> read() = 0;
 };
 
 }  // namespace hyped::sensors
