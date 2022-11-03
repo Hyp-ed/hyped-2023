7fd149797bbf1b92773719d3a4b6aa9d096a0c3b
Kshitij Sharma <kshitij4sharma@gmail.com>
2022-11-03 18:57:25 +0000

    Rename function

--- test/navigation/navigation.cpp
+++ test/navigation/navigation.cpp
@@ -5,39 +5,39 @@
 
 namespace hyped::test {
 
-bool is_float_eq(float a, float b) {
-    return ((a - b) < core::kEpsilon) && ((b - a) < core::kEpsilon);
+bool is_float_eq(float a, float b)
+{
+  return ((a - b) < core::kEpsilon) && ((b - a) < core::kEpsilon);
 }
 
-void checkTrajectoriesEqual(const core::Trajectory &trajectory1,
-                            const core::Trajectory &trajectory2, bool equality)
+void checkTrajectoriesEquality(const core::Trajectory &trajectory1,
+                               const core::Trajectory &trajectory2,
+                               bool equality)
 {
   const bool comparision = is_float_eq(trajectory1.acceleration, trajectory2.acceleration)
-                        && is_float_eq(trajectory1.velocity, trajectory2.velocity)
-                        && is_float_eq(trajectory1.displacement, trajectory2.displacement);
+                           && is_float_eq(trajectory1.velocity, trajectory2.velocity)
+                           && is_float_eq(trajectory1.displacement, trajectory2.displacement);
   ASSERT_EQ(equality, comparision);
 }
-
-
 
 TEST(Trajectory, equality)
 {
   const core::Trajectory reference = {100.0, 10.0, 1.0};
   {
     const core::Trajectory other = {100.0, 10.0, 1.0};
-    checkTrajectoriesEqual(reference, other, true);
+    checkTrajectoriesEquality(reference, other, true);
   }
   {
     const core::Trajectory other = {200.0, 10.0, 1.0};
-    checkTrajectoriesEqual(reference, other, false);
+    checkTrajectoriesEquality(reference, other, false);
   }
   {
     core::Trajectory other = {100.0, 20.0, 1.0};
-    checkTrajectoriesEqual(reference, other, false);
+    checkTrajectoriesEquality(reference, other, false);
   }
   {
     core::Trajectory other = {100.0, 10.0, 2.0};
-    checkTrajectoriesEqual(reference, other, false);
+    checkTrajectoriesEquality(reference, other, false);
   }
 }
 
