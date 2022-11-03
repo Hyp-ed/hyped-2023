f0cc8b1b89ae84eb43fe46168d63ea14cc5d0604
SnickeyX <s2152674@ed.ac.uk>
2022-11-03 18:30:43 +0000

    accelerometer skele code

---
+++ lib/sensors/accelerometer.cpp
@@ -0,0 +1,28 @@
+#include "accelerometer.hpp"
+
+namespace hyped::sensors {
+
+    Accelerometer::Accelerometer(io::I2c &i2c, core::ILogger &log)
+        : log_(log), i2c_(i2c)
+    {
+    }
+
+    Accelerometer::~Accelerometer()
+    {
+    }
+
+    std::optional<float> Accelerometer::read() 
+    {
+        // TODOlater: read from i2c
+        log_.log(hyped::core::LogLevel::kFatal, "Accelerometer read not implemented");
+        return std::nullopt;
+    }
+
+    bool Accelerometer::configure()
+    {
+        // TODOlater: configure i2c
+        log_.log(hyped::core::LogLevel::kFatal, "Accelerometer configure not implemented");
+        return false; 
+    }
+
+} // namespace hyped::sensors
---
+++ lib/sensors/accelerometer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "sensors.hpp"
+
+#include <cstdint>
+#include <cstdio>
+#include <optional>
+
+#include <core/logger.hpp>
+#include <io/i2c.hpp>
+
+namespace hyped::sensors {
+
+class Accelerometer : II2cSensor<float> {
+  public:
+    Accelerometer(io::I2c &i2c, hyped::core::ILogger &log);
+    ~Accelerometer();
+
+    bool configure(); 
+    std::optional<float> read();
+
+  private:
+    hyped::core::ILogger &log_; 
+    hyped::io::I2c &i2c_; // I2c object for the temperature sensor
+
+};
+
+}  // namespace hyped::sensors
