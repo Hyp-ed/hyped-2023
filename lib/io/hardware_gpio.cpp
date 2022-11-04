258b43caa39c36ebb7f3d9fcefa7a9c7c6f90709
root <root@LAPTOP-GQ4V9SLG.localdomain>
2022-11-04 23:29:25 +0000

    Initial

--- lib/io/hardware_gpio.cpp
+++ lib/io/hardware_gpio.cpp
@@ -1,16 +1,36 @@
 #include "hardware_gpio.hpp"
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+
 
 namespace hyped::io {
 
 std::optional<core::DigitalSignal> HardwareGpioReader::read()
 {
-  // TODO: implement
-  throw -1;
+  
+  int out = *gpio_readAddr & pinMAP;
+  if (out > 0) {
+    return core::DigitalSignal::kHigh;
+  } else{
+    return core::DigitalSignal::kLow;
+  }
 }
 
 GpioWriteResult HardwareGpioWriter::write(const core::DigitalSignal state)
 {
-  // TODO: implement
+  //Not sure if this correct
+  //May just erase everything
+  //May need to 
+  if (state == core::DigitalSignal::kHigh) {
+    *gpio_setAddr |= pinMAP;
+  } else {
+    *gpio_clearAddr &= ~pinMAP;
+  }
+
   throw -1;
 }
 
@@ -19,18 +39,57 @@ HardwareGpio::HardwareGpio(hyped::core::ILogger &log) : log_(log)
   // TODO: implement
 }
 
+
+
 std::optional<std::shared_ptr<IGpioReader>> HardwareGpio::getReader(const uint8_t pin)
 {
-  // TODO: implement
-  log_.log(hyped::core::LogLevel::kFatal, "GPIO reader not implemented");
-  return std::nullopt;
+  //What happens if all shared pointers are removed and this is called?
+  if (InitializedReaders.count(pin) != 0) {
+    std::shared_ptr<HardwareGpioReader> reader;
+    return reader;
+  }
+  const uint8_t bank = pin / 32;
+  const uint8_t pinID = pin % 32;
+  const uint8_t pinMAP = (1 << pinID);
+
+  const off_t pinAddress = bankAddresses[bank];
+  volatile void *gpio_addr;
+  volatile unsigned int *gpio_read;
+
+
+  int fd = open("/dev/mem", O_RDWR);
+  gpio_addr = mmap(0, pinSize , PROT_READ | PROT_WRITE, MAP_SHARED, fd, pinAddress);
+  gpio_read = static_cast<volatile unsigned int*>(gpio_addr) + pinRead;
+  
+  auto reader = std::shared_ptr<HardwareGpioReader>(new HardwareGpioReader(pinMAP, gpio_read));
+  InitializedReaders[pin] = true;
+  return reader;
 }
 
 std::optional<std::shared_ptr<IGpioWriter>> HardwareGpio::getWriter(const uint8_t pin)
 {
-  // TODO: implement
-  log_.log(hyped::core::LogLevel::kFatal, "GPIO writer not implemented");
-  return std::nullopt;
+  if (InitializedWriters.count(pin) != 0) {
+    std::shared_ptr<HardwareGpioWriter> writer;
+    return writer;
+  }
+
+
+  const uint8_t bank = pin / 32;
+  const uint8_t pinID = pin % 32;
+  const uint8_t pinMAP = (1 << pinID);
+
+  const off_t pinAddress = bankAddresses[bank];
+  volatile void *gpio_addr;
+  volatile unsigned int *gpio_set;
+  volatile unsigned int *gpio_clear;
+
+  int fd = open("/dev/mem", O_RDWR);
+  gpio_addr = mmap(0, pinSize , PROT_READ | PROT_WRITE, MAP_SHARED, fd, pinAddress);
+  gpio_set =   static_cast<volatile unsigned int*>(gpio_addr) + pinSet;
+  gpio_clear = static_cast<volatile unsigned int*>(gpio_addr) + pinClear;
+  auto writer = std::shared_ptr<HardwareGpioWriter>(new HardwareGpioWriter(pinMAP, gpio_set, gpio_clear));
+  InitializedWriters[pin] = true;
+  return writer;
 }
 
 }  // namespace hyped::io
--- lib/io/hardware_gpio.hpp
+++ lib/io/hardware_gpio.hpp
@@ -3,6 +3,7 @@
 #include "gpio.hpp"
 
 #include <core/types.hpp>
+#include <unordered_map>
 
 namespace hyped::io {
 
@@ -11,16 +12,31 @@ class HardwareGpioReader : public IGpioReader {
   virtual std::optional<core::DigitalSignal> read();
 
  private:
-  HardwareGpioReader();
+  HardwareGpioReader(uint8_t pin, volatile unsigned int *read): pinMAP(pin), gpio_readAddr(read){};
+
+  const uint8_t pinMAP;
+  volatile unsigned int *gpio_readAddr;
+
   friend class HardwareGpio;
 };
+
+
+
+
+
+
 
 class HardwareGpioWriter : public IGpioWriter {
  public:
   virtual GpioWriteResult write(const core::DigitalSignal state);
 
  private:
-  HardwareGpioWriter(const uint8_t pin);
+  HardwareGpioWriter(const uint8_t pin,  volatile unsigned int *set, volatile unsigned int *clear): pinMAP(pin), gpio_setAddr(set), gpio_clearAddr(clear){};
+  
+  const uint8_t pinMAP;
+  volatile unsigned int *gpio_setAddr;
+  volatile unsigned int *gpio_clearAddr;
+
   friend class HardwareGpio;
 };
 
@@ -28,6 +44,21 @@ class HardwareGpioWriter : public IGpioWriter {
  * Hardware GPIO interface, requires physical GPIO pins to be present. This should only
  * be instantiated at the top level and then provided to users through the IGpio interface.
  */
+
+//GPIO  Start Addr   End Addr
+//GPIO0 0x44E0_7000  0x44E0_7FFF
+//GPIO1 0x4804_C000  0x4804_CFFF
+//GPIO2 0x481A_C000  0x481A_CFFF
+//GPIO3 0x481A_E000  0x481A_EFFF
+
+//GPIO_DATAIN (READ) 0x138h
+//GPIO_DATAOUT 0x13c
+//SET 0x194
+//CLEAR 0x190
+//  const uint8_t bank   = pin_ / 32;  // offset: GPIO_0,1,2,3
+//  const uint8_t pin_id = pin_ % 32;
+
+
 class HardwareGpio {
  public:
   HardwareGpio(hyped::core::ILogger &log);
@@ -37,6 +68,14 @@ class HardwareGpio {
 
  private:
   hyped::core::ILogger &log_;
+  const off_t bankAddresses[4] = {0x44e07000, 0x4804c000, 0x481ac000, 0x481ae000};
+  std::unordered_map<uint8_t, bool> InitializedWriters;
+  std::unordered_map<uint8_t, bool> InitializedReaders;
+  
+  static constexpr unsigned int pinSize     = 0x1000;
+  static constexpr unsigned int pinRead     = 0x138;
+  static constexpr unsigned int pinClear    = 0x190;
+  static constexpr unsigned int pinSet      = 0x194;
 };
 
 }  // namespace hyped::io
