a1b5d98b9ef19b0111cc3f49f8c437d47bd14b8d
BenET01 <s2055565@ed.ac.uk>
2022-11-08 11:55:23 +0000

    filtered outliers and fixed naming

--- lib/navigation/preprocess_imu.cpp
+++ lib/navigation/preprocess_imu.cpp
@@ -1,5 +1,7 @@
 #include "preprocess_imu.hpp"
 
+#include <cmath>
+
 namespace hyped::navigation
 {
 
@@ -35,7 +37,7 @@ namespace hyped::navigation
     -also figure out return type/ what we update and update
     documentation as appropriate
     */
-    core::ImuData clean_imu_data;
+    core::ImuData clean_accelerometer_data;
     core::Float magnitude;
     for (size_t i = 0; i < core::kNumImus; ++i)
     {
@@ -44,16 +46,53 @@ namespace hyped::navigation
       {
         magnitude += std::pow(imu_data.at(i).at(j), 2);
       }
-      // TODO change to sqrt function
-      clean_imu_data.at(i) = std::pow(magnitude, 0.5);
+      clean_accelerometer_data.at(i) = std::sqrt(magnitude);
     }
-    const uint8_t num_reliable_imus = std::accumulate(are_imus_reliable_.begin(), are_imus_reliable_.end(), 0);
-    if (num_reliable_imus == 4)
-    {
-      std::sort(imu_data.start(), imu_data.end());
-      const core::Float q1 = (imu_data.at(0) + imu_data.at(1)) / 2.0;
-      const core::Float median = (imu_data.at(1) + imu_data.at(2)) / 2.0;
-      const core::Float q3 = (imu_data.at(2) + imu_data.at(3)) / 2.0;
+    const uint8_t num_reliable_accelerometers = std::accumulate(are_imus_reliable_.begin(), are_imus_reliable_.end(), 0);
+    if (num_reliable_accelerometers == 4)
+    { 
+      core::ImuData clean_accelerometer_data_copy = std::copy(clean_accelerometer_data);
+      std::sort(clean_accelerometer_data_copy.begin(), clean_accelerometer_data_copy.end());
+      const core::Float q1 = (clean_accelerometer_data_copy.at(0) + clean_accelerometer_data_copy.at(1)) / 2.0;
+      const core::Float median = (clean_accelerometer_data_copy.at(1) + clean_accelerometer_data_copy.at(2)) / 2.0;
+      const core::Float q3 = (clean_accelerometer_data_copy.at(2) + clean_accelerometer_data_copy.at(3)) / 2.0;
+      const core::Float iqr = q3 - q1;
+
+      for (int i = 0; i < core::kNumImus; ++i) {
+        if (clean_accelerometer_data.at(i) > q3 + 1.5*iqr || clean_accelerometer_data.at(i) < q1 - 1.5*iqr) {
+            clean_accelerometer_data.at(i) = median;
+            num_outliers_per_imu_.at(i) += 1;
+        } else {
+          num_outliers_per_imu_.at(i) = 0;
+        }
+      }
+    } else if (num_reliable_accelerometers == 3) {
+      core::ImuData clean_accelerometer_data_copy = std::copy(clean_accelerometer_data);
+      for (int i = 0; i < core::kNumImus; ++i) {
+        if (are_imus_reliable_.at(i) == false) {
+          clean_accelerometer_data_copy.at(i) = -1;
+        }
+      }
+      std::sort(clean_accelerometer_data_copy.begin(), clean_accelerometer_data_copy.end());
+      const core::Float q1 = (clean_accelerometer_data_copy.at(1) + clean_accelerometer_data_copy.at(2)) / 2.0;
+      const core::Float median = clean_accelerator_data_copy.at(2);
+      const core::Float q3 = (clean_accelerometer_data_copy.at(2) + clean_accelerometer_data_copy.at(3)) / 2.0;
+      const core::Float iqr = q3 - q1;
+      
+      for (int i = 0; i < core::kNumImus; ++i) {
+        if (are_imus_reliable_.at(i) == false) {
+          clean_accelerometer_data.at(i) = median;
+        } else {
+          if (clean_accelerometer_data.at(i) > q3 + 1.5*iqr || clean_accelerometer_data.at(i) < q1 - 1.5*iqr) {
+            clean_accelerometer_data.at(i) = median;
+            num_outliers_per_imu_.at(i) += 1;
+          } else {
+            num_outliers_per_imu_.at(i) = 0;
+          }
+        }
+      }
+    } else {
+      // TODO check Fail state implementation
     }
     return {0, 0, 0, 0};
   }
