d0e45fca771dcfae7281ce17d955d3ab00b93d83
aryansharma19 <aryansharma2119@gmail.com>
2022-11-08 15:47:29 +0000

    calculating the value of q1,q3 and median for detectOutliers function

--- lib/navigation/preprocess_encoders.cpp
+++ lib/navigation/preprocess_encoders.cpp
@@ -1,7 +1,9 @@
 #include "preprocess_encoders.hpp"
 #include "core/types.hpp"
-
-
+#include <iterator>
+#include <algorithm>
+#include <array>
+#include <vector>
 namespace hyped::navigation {
 
 EncodersPreprocessor::EncodersPreprocessor()
@@ -36,7 +38,37 @@ core::EncoderData EncodersPreprocessor::detectOutliers(const core::EncoderData e
   -also figure out return type/ what we update and update
   documentation as appropriate
   */
- std::sort(encoder_data);
+ core::EncoderData encoder_data_copy = std::copy(encoder_data.begin(), encoder_data.end(), std::back_inserter(encoder_data));
+ std::sort(encoder_data_copy.begin(), encoder_data_copy.end());
+ core::Float median = 0;
+ core::Float q1 = 0.0;
+ core::Float q3 = 0.0;
+ if(encoder_data_copy.size() % 2 == 0){
+  median = (encoder_data_copy[(std::size(encoder_data_copy))/2] + encoder_data_copy[((std::size(encoder_data_copy))/2) + 1])/2.0;
+ }
+ else{
+  median = encoder_data_copy[(std::size(encoder_data_copy) + 1)/2];
+ }
+ core::EncoderData array_for_q1 ;
+ std::copy_if(encoder_data_copy.begin(),encoder_data_copy.end(),array_for_q1.begin(),[](uint32_t i){return i< median;});
+ core::EncoderData array_for_q2;
+ std::copy_if(encoder_data_copy.begin(),encoder_data_copy.end(),array_for_q2.begin(),[](uint32_t i){return i> median;});
+ if(array_for_q1.size() % 2 == 0){
+  q1 = (array_for_q1[(array_for_q1.size())/2] +array_for_q1[((array_for_q1.size())/2) + 1] )/2.0;
+ }
+ else{
+  q1 = array_for_q1[(array_for_q1.size()+1)/2.0];
+ }
+ if(array_for_q2.size() % 2 == 0){
+  q3 = (array_for_q2[(array_for_q2.size()/2)] + array_for_q2[((array_for_q2.size()/2) + 1)])/2.0;
+ }
+ else{
+  q3 = array_for_q2[(array_for_q2.size() + 1)]/2.0;
+ }
+
+ //const core::Float q1 = (encoder_data_copy.at(0) + encoder_data_copy.at(1))/2.0;
+ //const core::Float q3 = (encoder_data_copy.at(2) + encoder_data_copy.at(3))/2.0;
+ const core::Float iqr = q3-q1;
  return {0, 0, 0, 0};
 }
 
