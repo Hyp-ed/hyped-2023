bcb905fe8393a81296b1837e48d73761d97561ce
HTTYDKing <s2335450@ed.ac.uk>
2022-11-09 15:56:27 +0000

    Work in progress

--- lib/sensors/temperature.cpp
+++ lib/sensors/temperature.cpp
@@ -8,9 +8,9 @@ Temperature::Temperature(hyped::io::I2c &i2c, hyped::core::ILogger &log) : log_(
 
 std::optional<std::uint16_t> Temperature::read()
 {
-  if (I2c::readByte(TEMPERATURE_1, STATUS) == 0) {
-    auto Temp_High = I2c::readByte(TEMPERATURE_1, TEMP_H);
-    auto Temp_Low  = I2c::readByte(TEMPERATURE_1, TEMP_L);
+  if (i2c_.readByte(TEMPERATURE_1, STATUS) == 0) {
+    auto Temp_High = i2c_.readByte(TEMPERATURE_1, TEMP_H);
+    auto Temp_Low  = i2c_.readByte(TEMPERATURE_1, TEMP_L);
     // getting the high and low values from the register
 
     auto Temp = (TEMP_H << 8) | TEMP_L;
@@ -19,15 +19,15 @@ std::optional<std::uint16_t> Temperature::read()
     return std::optional<std::uint16_t>{Temp};
     // Reading from the Status register to see if the tempurature is ready to be read
     // Need to have it do the correct effect and output a log of what occurred
-  }; else {
+  } else {
     return std::nullopt;
-  };
+  }
   // log_.log(hyped::core::LogLevel::kFatal, "Temperature read not implemented");
 }
 
 bool Temperature::configure()
 {
-  I2c::writeByte(TEMPERATURE_1, CTRL, 1);
+  i2c_.writeByte(TEMPERATURE_1, CTRL, 1);
   // We need to know if a begalebone has two registers set to it
 
   // Writing to the Control register 0x04 and setting that to 1 for TEMPERATURE_1
