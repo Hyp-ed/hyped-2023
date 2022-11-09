986329309c101f58e14602b6bd244b12144fc487
Vladimir Hanin <vladimir.hanin@outlook.com>
2022-11-09 15:19:08 +0000

    WIP

--- lib/sensors/accelerometer.cpp
+++ lib/sensors/accelerometer.cpp
@@ -13,16 +13,43 @@ namespace hyped::sensors {
 
     std::optional<float> Accelerometer::read() 
     {
-        // TODOlater: read from i2c
-        log_.log(hyped::core::LogLevel::kFatal, "Accelerometer read not implemented");
-        return std::nullopt;
+        
+        do
+		{
+			/*Wait till the value is ready to read*/
+			ITDS_getdataReadyState(&DRDY);
+		} while (DRDY == ITDS_disable);
+
+		ITDS_getRawAccelerationX(&XRawAcc);
+		XRawAcc = XRawAcc >> 2; /* shifted by 2 as 14bit resolution is used in high performance mode */
+		float XAcceleration = (float) (XRawAcc);
+		XAcceleration = XAcceleration / 1000; /* mg to g */
+		XAcceleration = XAcceleration * 1.952; /* Multiply with sensitivity 1.952 in high performance mode, 14bit, and full scale +-16g */
+		printf("Acceleration X-axis %f g \r\n ", XAcceleration);
+
+		ITDS_getRawAccelerationY(&YRawAcc);
+		YRawAcc = YRawAcc >> 2;
+		float YAcceleration = (float) (YRawAcc);
+		YAcceleration = YAcceleration / 1000;
+		YAcceleration = (YAcceleration * 1.952);
+		printf("Acceleration Y-axis %f g \r\n ", YAcceleration);
+
+		ITDS_getRawAccelerationZ(&ZRawAcc);
+		ZRawAcc = ZRawAcc >> 2;
+		float ZAcceleration = (float) (ZRawAcc);
+		ZAcceleration = ZAcceleration / 1000;
+		ZAcceleration = ZAcceleration * 1.952;
+		printf("Acceleration Z-axis %f g \r\n ", ZAcceleration);
+
+       
     }
 
     bool Accelerometer::configure()
     {
-        // TODOlater: configure i2c
-        log_.log(hyped::core::LogLevel::kFatal, "Accelerometer configure not implemented");
-        return false; 
+        i2c.writeByte(ADDR, 0x20, 0x64);
+        i2c.writeByte(ADDR, 0x21, 0xC);
+        i2c.writeByte(ADDR, 0x25, 0x30);
+        return true; 
     }
 
 } // namespace hyped::sensors
--- lib/sensors/accelerometer.hpp
+++ lib/sensors/accelerometer.hpp
@@ -9,20 +9,24 @@
 #include <core/logger.hpp>
 #include <io/i2c.hpp>
 
-namespace hyped::sensors {
+namespace hyped::sensors
+{
 
-class Accelerometer : II2cSensor<float> {
+  const uint8_t ADDR = 0x19;
+
+
+  class Accelerometer : II2cSensor<float>
+  {
   public:
     Accelerometer(io::I2c &i2c, hyped::core::ILogger &log);
     ~Accelerometer();
 
-    bool configure(); 
+    bool configure();
     std::optional<float> read();
 
   private:
-    hyped::core::ILogger &log_; 
-    hyped::io::I2c &i2c_; // I2c object for the temperature sensor
+    hyped::core::ILogger &log_;
+    hyped::io::I2c &i2c_; // I2c object for the accelerometer
+  };
 
-};
-
-}  // namespace hyped::sensors
+} // namespace hyped::sensors
