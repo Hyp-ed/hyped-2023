9e658941fabaf2605cc4d9c90fd9c104e6b7a072
root <root@LAPTOP-V72QHATH.localdomain>
2022-11-09 15:19:30 +0000

    Modified the Tempurature sensor code

--- lib/sensors/temperature.cpp
+++ lib/sensors/temperature.cpp
@@ -8,7 +8,6 @@ Temperature::Temperature(hyped::io::I2c &i2c, hyped::core::ILogger &log) : log_(
 
     std::optional<uint16_t> Temperature::read() 
     {
-        // TODOlater: read from i2c
         if (I2c::readByte(TEMPERATURE_1, STATUS) == 0 ){
 
         auto Temp_High = I2c::readByte(TEMPERATURE_1, TEMP_H);
@@ -20,7 +19,7 @@ Temperature::Temperature(hyped::io::I2c &i2c, hyped::core::ILogger &log) : log_(
 
         return std::optional<uint16_t>{Temp};
         //Reading from the Status register to see if the tempurature is ready to be read
-        //TODO have it do the correct effect and output a log of what occurred
+        //Need to have it do the correct effect and output a log of what occurred
         };
         else{
             return std::nullopt;
@@ -32,19 +31,13 @@ Temperature::Temperature(hyped::io::I2c &i2c, hyped::core::ILogger &log) : log_(
     {
 
         I2c::writeByte(TEMPERATURE_1, CTRL, 1);
-        // TODO We need to know if a begalebone has two registers set to it
+        // We need to know if a begalebone has two registers set to it
+        
         //Writing to the Control register 0x04 and setting that to 1 for TEMPERATURE_1
 
 
         //log_.log(hyped::core::LogLevel::kFatal, "Temperature configure not implemented");
         return true; 
     }
-
-bool Temperature::configure()
-{
-  // TODOLater: configure i2c
-  log_.log(hyped::core::LogLevel::kFatal, "Temperature configure not implemented");
-  return false;
-}
 
 }  // namespace hyped::sensors
