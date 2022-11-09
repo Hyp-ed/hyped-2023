aec3ef568f0691af011e6c05bad2a2f463b9399a
Vladimir Hanin <vladimir.hanin@outlook.comclear>
2022-11-09 17:53:06 +0000

    finished main structure, only return type is not good

--- lib/sensors/accelerometer.cpp
+++ lib/sensors/accelerometer.cpp
@@ -2,7 +2,7 @@
 
 namespace hyped::sensors {
 
-Accelerometer::Accelerometer(io::I2c &i2c, core::ILogger &log) : log_(log), i2c_(i2c)
+Accelerometer::Accelerometer(std::uint8_t mux_address, io::I2c &i2c, core::ILogger &log) : ADDR(mux_address), log_(log), i2c_(i2c)
 {
 }
 
@@ -10,44 +10,94 @@ Accelerometer::~Accelerometer()
 {
 }
 
+
+    std::optional<std::int16_t> getRawAccelerationX() {
+
+		std::optional<std::uint8_t> resultLow = i2c_.readByte(ADDR, 0x28);
+		std::optional<std::uint8_t> resultHigh = i2c_.readByte(ADDR, 0x29);
+
+		if (!resultLow.hasValue() || !resultLow.hasValue()) {
+			return std::nullopt;
+		}
+
+		std::optional<std::int16_t> rawX = (resultHigh.value() << 8) | resultLow.value();
+
+		return rawX;
+	}
+
+
+    std::optional<std::int16_t> getRawAccelerationY() {
+
+		std::optional<std::uint8_t> resultLow = i2c_.readByte(ADDR, 0x2A);
+		std::optional<std::uint8_t> resultHigh = i2c_.readByte(ADDR, 0x2B);
+
+		if (!resultLow.hasValue() || !resultLow.hasValue()) {
+			return std::nullopt;
+		}
+
+		std::optional<std::int16_t> rawX = (resultHigh.value() << 8) | resultLow.value();
+
+		return rawX;
+	}
+
+
+    std::optional<std::int16_t> getRawAccelerationZ() {
+
+		std::optional<std::uint8_t> resultLow = i2c_.readByte(ADDR, 0x2C);
+		std::optional<std::uint8_t> resultHigh = i2c_.readByte(ADDR, 0x2D);
+
+		if (!resultLow.hasValue() || !resultLow.hasValue()) {
+			return std::nullopt;
+		}
+
+		std::optional<std::int16_t> rawX = (resultHigh.value() << 8) | resultLow.value();
+
+		return rawX;
+	}
+
     std::optional<float> Accelerometer::read() 
     {
         
-        do
-		{
-			/*Wait till the value is ready to read*/
-			ITDS_getdataReadyState(&DRDY);
-		} while (DRDY == ITDS_disable);
+        /* check to see if the values are ready to be read*/
+		std::optional<std::uint8_t> result = i2c_.readByte(ADDR, 0x27);
+		
+		// todoLater: probably have to differentiate between the two tests. The reading failing completely might need to retry configuring the sensor
+		if (!result.hasValue() || result.value() == 0x00) {
+			return std::nullopt;
+		}	
 
-		ITDS_getRawAccelerationX(&XRawAcc);
-		XRawAcc = XRawAcc >> 2; /* shifted by 2 as 14bit resolution is used in high performance mode */
+		std::optional<std::int16_t> resultX = getRawAccelerationX();
+		if (!resultX.hasValue()) return std::nullopt;
+		std::int16_t XRawAcc = resultX.value() >> 2; /* shifted by 2 as 14bit resolution is used in high performance mode */
 		float XAcceleration = (float) (XRawAcc);
 		XAcceleration = XAcceleration / 1000; /* mg to g */
 		XAcceleration = XAcceleration * 1.952; /* Multiply with sensitivity 1.952 in high performance mode, 14bit, and full scale +-16g */
 		printf("Acceleration X-axis %f g \r\n ", XAcceleration);
 
-		ITDS_getRawAccelerationY(&YRawAcc);
-		YRawAcc = YRawAcc >> 2;
+		std::optional<std::int16_t> resultY = getRawAccelerationY();
+		if (!resultY.hasValue()) return std::nullopt;
+		std::int16_t YRawAcc = resultY.value() >> 2;
 		float YAcceleration = (float) (YRawAcc);
 		YAcceleration = YAcceleration / 1000;
 		YAcceleration = (YAcceleration * 1.952);
 		printf("Acceleration Y-axis %f g \r\n ", YAcceleration);
 
-		ITDS_getRawAccelerationZ(&ZRawAcc);
-		ZRawAcc = ZRawAcc >> 2;
+		std::optional<std::int16_t> resultZ = getRawAccelerationZ();
+		if (!resultZ.hasValue()) return std::nullopt;
+		std::int16_t ZRawAcc = resultZ.value() >> 2;
 		float ZAcceleration = (float) (ZRawAcc);
 		ZAcceleration = ZAcceleration / 1000;
 		ZAcceleration = ZAcceleration * 1.952;
 		printf("Acceleration Z-axis %f g \r\n ", ZAcceleration);
 
-       
+		// todo: return the three values and the timestamp, ask navigation about this.
     }
 
     bool Accelerometer::configure()
     {
-        i2c.writeByte(ADDR, 0x20, 0x64);
-        i2c.writeByte(ADDR, 0x21, 0xC);
-        i2c.writeByte(ADDR, 0x25, 0x30);
+        i2c_.writeByte(ADDR, 0x20, 0x64);
+        i2c_.writeByte(ADDR, 0x21, 0xC);
+        i2c_.writeByte(ADDR, 0x25, 0x30);
         return true; 
     }
 
--- lib/sensors/accelerometer.hpp
+++ lib/sensors/accelerometer.hpp
@@ -12,13 +12,11 @@
 namespace hyped::sensors
 {
 
-  const uint8_t ADDR = 0x19;
-
 
   class Accelerometer : II2cSensor<float>
   {
   public:
-    Accelerometer(io::I2c &i2c, hyped::core::ILogger &log);
+    Accelerometer(const std::uint8_t mux_address, io::I2c &i2c, hyped::core::ILogger &log);
     ~Accelerometer();
 
     bool configure();
@@ -27,6 +25,11 @@ namespace hyped::sensors
   private:
     hyped::core::ILogger &log_;
     hyped::io::I2c &i2c_; // I2c object for the accelerometer
+    const std::unit8_t ADDR;
+
+    std::optional<std::int16_t> getRawAccelerationX();
+    std::optional<std::int16_t> getRawAccelerationY();
+    std::optional<std::int16_t> getRawAccelerationZ();
   };
 
 } // namespace hyped::sensors
