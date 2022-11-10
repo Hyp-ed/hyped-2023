1ed0859cbea617def7caf311d39e353f65fec642
aryansharma19 <aryansharma2119@gmail.com>
2022-11-10 23:28:23 +0000

    implemented detectOutliers

--- lib/navigation/preprocess_encoders.cpp
+++ lib/navigation/preprocess_encoders.cpp
@@ -3,7 +3,7 @@
 #include <iterator>
 #include <algorithm>
 #include <array>
-#include <vector>
+
 namespace hyped::navigation {
 
 EncodersPreprocessor::EncodersPreprocessor()
@@ -46,15 +46,24 @@ core::EncoderData EncodersPreprocessor::detectOutliers(const core::EncoderData e
   encoder_data_copy.at(i) = encoder_data.at(i);
  }
  if(encoder_data.size() % 2 == 0){
-    median =  ((encoder_data.at(encoder_data.size()/2) + encoder_data.at((encoder_data.size()/2) + 1))/2.0);
+    median =  ((encoder_data.at(encoder_data.size()/2) + encoder_data.at((encoder_data.size()/2) + 1))/2.0);  //calculating the median of encoder data if the length of the array is even
+                                                                                                               
   }
   else{
-    median = encoder_data.at(encoder_data.size() + 1)/2.0;
+    median = encoder_data.at(encoder_data.size() + 1)/2.0; //median if the length of encoder's data is odd
   }
- core::Float q1 = encoder_data_copy.at(((encoder_data_copy.size() + 1)/4) -1);
- core::Float q3 = encoder_data_copy.at((3*(encoder_data_copy.size() + 1)/4) -1);
+ core::Float q1 = encoder_data_copy.at(((encoder_data_copy.size() + 1)/4) -1);    // Formula taken from net
+ core::Float q3 = encoder_data_copy.at((3*(encoder_data_copy.size() + 1)/4) -1);  // Formula tken from net
 
- return {0, 0, 0, 0};
+ core::EncoderData num_outliers_per_encoder;
+ for(int i =0;i<core::kNumEncoders;i++){
+  if(encoder_data_copy.at(i) > q3 || encoder_data_copy.at(i) < q1){
+    num_outliers_per_encoder.at(i) = num_outliers_per_encoder.at(i) + 1;
+    encoder_data_copy.at(i) = median;   // updating the value of outlier to the median
+  }
+ }
+
+ return {0, 0, 0, 0}; // might be a good idea to create an array of size 2 such that index 0 contains our updated encoder_data and the element at index 1 is an array of the number of outliers per encoder
 } 
 
 
