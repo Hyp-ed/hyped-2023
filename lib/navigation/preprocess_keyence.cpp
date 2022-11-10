1c48aab9d87e3f55cfbc8ef54661b097a7b49ef1
Fayz-S <fayz.siddiqui@outlook.com>
2022-11-10 10:30:11 +0000

    Formatting changes to investigate ubuntu build failure.

--- lib/navigation/preprocess_keyence.cpp
+++ lib/navigation/preprocess_keyence.cpp
@@ -2,7 +2,7 @@
 
 namespace hyped::navigation {
 
-KeyencePreprocessor::KeyencePreprocessor(hyped::core::ILogger &logger) : logger_(logger)
+KeyencePreprocessor::KeyencePreprocessor(hyped::core::ILogger &logger) : log_(logger)
 {
   // Currently no extra parameters so constructor is empty
   has_keyence_disagreed_ = false;
@@ -27,14 +27,18 @@ SensorChecks KeyencePreprocessor::checkKeyenceAgrees(const core::KeyenceData &ke
   for (size_t i = 0; i < keyence_data.size() - 1; ++i) {
     if (keyence_data.at(i) != keyence_data.at(i + 1)) { keyence_data_disagrees = true; }
   }
+
   if (keyence_data_disagrees && has_keyence_disagreed_) {
     log_.log(hyped::core::LogLevel::kFatal, "Keyence disagreed more than once");
     return SensorChecks::kUnacceptable;
+
   } else if (keyence_data_disagrees && !(has_keyence_disagreed_)) {
     has_keyence_disagreed_ = true;
+
   } else if (!(keyence_data_disagrees) && has_keyence_disagreed_) {
     has_keyence_disagreed_ = false;
   }
+
   return SensorChecks::kAcceptable;
 }
 
