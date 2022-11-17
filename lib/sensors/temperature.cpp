21eaa5f0e54f5c7639ea1fd197bb9ebb2625e3f1
HTTYDKing <s2335450@ed.ac.uk>
2022-11-17 19:41:18 +0000

    Version 3 for tempurature sensor with corrections to variable names

--- lib/sensors/temperature.cpp
+++ lib/sensors/temperature.cpp
@@ -8,34 +8,46 @@ Temperature::Temperature(hyped::io::I2c &i2c, hyped::core::ILogger &log) : log_(
 
 std::optional<std::uint16_t> Temperature::read()
 {
-  if (i2c_.readByte(TEMPERATURE_1, STATUS) == 0) {
-    const std::optional<std::uint8_t> Tempurature_High = i2c_.readByte(kMUX, TEMP_H);
-    const std::optional<std::uint8_t> Tempurature_Low  = i2c_.readByte(kMUX, TEMP_L);
-    // getting the high and low values from the register
+  const auto Status_check = i2c_.readByte(kMux, kStatus);
 
-    if (Tempurature_High = !std::nullopt, Tempurature_Low = !std::nullopt) {
-      std::optional<std::uint16_t> Tempurature = (Tempurature_High << 8) | Tempurature_Low;
-      Tempurature                              = Tempurature * 0.01;
+  if (Status_check) {
+    if (Status_check.value() == 0) {
+      const std::optional<std::uint8_t> tempurature_high = i2c_.readByte(kMux, kTempuratureHigh);
+      const std::optional<std::uint8_t> tempurature_low  = i2c_.readByte(kMux, kTempuratureLow);
+      // getting the high and low values from the register
 
-      return Tempurature;
+      if (tempurature_high.has_value() && tempurature_low.has_value()) {
+        std::uint16_t tempurature = (tempurature_high.value() << 8) | tempurature_low.value();
+        tempurature               = tempurature * 0.01;
+
+        return tempurature;
+      } else {
+        log_.log(hyped::core::LogLevel::kFatal, "Temperature read not implemented");
+        return std::nullopt;
+      }
+      // Reading from the Status register to see if the tempurature is ready to be read
+      // Need to have it do the correct effect and output a log of what occurred
+
     } else {
       log_.log(hyped::core::LogLevel::kFatal, "Temperature read not implemented");
+      return std::nullopt;
     }
-    // Reading from the Status register to see if the tempurature is ready to be read
-    // Need to have it do the correct effect and output a log of what occurred
+  } else {
+    log_.log(hyped::core::LogLevel::kFatal, "Temperature read not implemented");
+    return std::nullopt;
   }
-  // log_.log(hyped::core::LogLevel::kFatal, "Temperature read not implemented");
 }
 
-bool Temperature::configure()
+core::Result Temperature::configure()
 {
-  hyped::core::Result::Check = i2c_.writeByte(kMUX, CTRL, 1);
+  const core::Result write_result = i2c_.writeByteToRegister(kMux, kControl, 1);
   // We need to know if a begalebone has two registers set to it
-  if (Check == hyped::core::Result::kFailure) { return false; }
+  if (write_result == hyped::core::Result::kFailure) {
+    log_.log(hyped::core::LogLevel::kFatal, "Temperature configure not implemented");
+    return core::Result::kFailure;
+  }
   // Writing to the Control register 0x04 and setting that to 1 for TEMPERATURE_1
-
-  // log_.log(hyped::core::LogLevel::kFatal, "Temperature configure not implemented");
-  return true;
+  return core::Result::kSuccess;
 }
 
 }  // namespace hyped::sensors
--- lib/sensors/temperature.hpp
+++ lib/sensors/temperature.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "sensors.hpp"
+#include "i2c_sensors.hpp"
 
 #include <cstdint>
 #include <cstdio>
@@ -11,18 +11,18 @@
 
 namespace hyped::sensors {
 
-static constexpr std::uint8_t kMUX    = 0x70;
-static constexpr std::uint8_t kCTRL   = 0x04;
-static constexpr std::uint8_t kTEMP_H = 0x07;
-static constexpr std::uint8_t kTEMP_L = 0x06;
-static constexpr std::uint8_t kSTATUS = 0x05;
+static constexpr std::uint8_t kMux             = 0x70;
+static constexpr std::uint8_t kControl         = 0x04;
+static constexpr std::uint8_t kTempuratureHigh = 0x07;
+static constexpr std::uint8_t kTempuratureLow  = 0x06;
+static constexpr std::uint8_t kStatus          = 0x05;
 
-class Temperature : public II2cSensor<std::uint16_t> {
+class Temperature : public II2cMuxSensor<std::uint16_t> {
  public:
   Temperature(hyped::io::I2c &i2c, hyped::core::ILogger &log);
   ~Temperature();
 
-  bool configure();
+  core::Result configure();
   std::optional<std::uint16_t> read();
 
  private:
