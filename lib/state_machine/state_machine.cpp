aa038a0a2457a1215a84089c2c0abd63ddbd2f93
DylanCavers <s2179041@ed.ac.uk>
2022-11-21 18:37:09 +0000

    small update

--- lib/state_machine/state_machine.cpp
+++ lib/state_machine/state_machine.cpp
@@ -15,13 +15,19 @@ namespace hyped::state_machine {
         //update data
         //should check if a transition is needed with current data
         //then run transition to move to next state
+        //return (None || transition_message)
     }
 
     //Transition to next state
     // TODO implement:
     void StateMachine::transition()
     {
-        //takes in a state and changes the current state to that state
+        //make the corresponding transition based on the transition message and the current state
+        //for (int i = 0; i < length(self.transitions; i++)) {
+        //  if (transitions[i].message == message && transitions[i].start == self.state) {
+        //      self.state = transitions[i].end;
+        //  }
+        //}
     }
 
 } //namespace hyped::state_machine
