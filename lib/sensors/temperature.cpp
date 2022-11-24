862eda6b72869b8a884db5fd3dfb3daf88b6547d
SnickeyX <s2152674@ed.ac.uk>
2022-11-24 20:14:14 +0000

    fixing requested changes

--- lib/sensors/temperature.cpp
+++ lib/sensors/temperature.cpp
@@ -2,14 +2,14 @@
 
 namespace hyped::sensors {
 
-Temperature::Temperature(const std::uint8_t mux_address,
+Temperature::Temperature(const std::uint8_t device_address,
                          const std::uint8_t channel,
                          hyped::io::I2c &i2c,
                          hyped::core::ILogger &log)
     : log_(log),
       i2c_(i2c),
       channel_(channel),
-      mux_address_(mux_address)
+      device_address_(device_address)
 {
 }
 
@@ -17,54 +17,40 @@ Temperature::~Temperature()
 {
 }
 
-std::optional<std::uint16_t> Temperature::read()
+std::optional<std::int16_t> Temperature::read()
 {
-  const auto status_check_result = i2c_.readByte(mux_address_, kStatus);
-
-  const auto temperature_high = i2c_.readByte(mux_address_, kData_T_H);
-  const auto temperature_low  = i2c_.readByte(mux_address_, kData_T_L);
-
-  // getting the temperature_high_byte and temperature_low_byte values from the register
-
+  const auto status_check_result = i2c_.readByte(device_address_, kStatus);
   if (!status_check_result) {
     log_.log(hyped::core::LogLevel::kFatal, "Temperature could not read status");
     return std::nullopt;
   }
-
   if (status_check_result.value() == 0) {
     log_.log(hyped::core::LogLevel::kFatal, "Temperature sensor is not ready to be read from");
     return std::nullopt;
   }
-
-  if (!temperature_high) {
+  const auto temperature_high_byte = i2c_.readByte(device_address_, kDataTH);
+  const auto temperature_low_byte  = i2c_.readByte(device_address_, kDataTL);
+  if (!temperature_high_byte) {
     log_.log(hyped::core::LogLevel::kFatal, "Temperature high could not be read");
     return std::nullopt;
   }
-
-  if (!temperature_low) {
+  if (!temperature_low_byte) {
     log_.log(hyped::core::LogLevel::kFatal, "Temperature low could not be read");
     return std::nullopt;
   }
-
-  std::uint16_t temperature = (temperature_high.value() << 8) | temperature_low.value();
-
-  temperature = temperature * 0.01;
-
-  return temperature;
-  // Reading from the Status register to see if the temperature is ready to be read
-  // Need to have it do the correct effect and output a log of what occurred
+  const auto temperature = ((temperature_high_byte.value() << 8) | temperature_low_byte.value());
+  // Scaling temperature as per the datasheet
+  return temperature * 0.01;
 }
 
 core::Result Temperature::configure()
 {
   const core::Result write_result
-    = i2c_.writeByteToRegister(mux_address_, kCtrl, kConfigurationSetting);
-  // We need to know if a begalebone has two registers set to it
+    = i2c_.writeByteToRegister(device_address_, kCtrl, kConfigurationSetting);
   if (write_result == hyped::core::Result::kFailure) {
     log_.log(hyped::core::LogLevel::kFatal, "Temperature configure not implemented");
     return core::Result::kFailure;
   }
-  // Writing to the Control register 0x04 and setting that to 1 for TEMPERATURE_1
   return core::Result::kSuccess;
 }
 
--- lib/sensors/temperature.hpp
+++ lib/sensors/temperature.hpp
@@ -11,28 +11,29 @@
 
 namespace hyped::sensors {
 
-static constexpr std::uint8_t kCtrl                 = 0x04;
-static constexpr std::uint8_t kData_T_H             = 0x07;
-static constexpr std::uint8_t kData_T_L             = 0x06;
-static constexpr std::uint8_t kStatus               = 0x05;
+static constexpr std::uint8_t kCtrl   = 0x04;
+static constexpr std::uint8_t kDataTH = 0x07;
+static constexpr std::uint8_t kDataTL = 0x06;
+static constexpr std::uint8_t kStatus = 0x05;
+// Sets the sensor to continuous mode, sets IF_ADD_INC, and sets sampling rate to 200Hz
 static constexpr std::uint8_t kConfigurationSetting = 0x3c;
 
-class Temperature : public II2cMuxSensor<std::uint16_t> {
+class Temperature : public II2cMuxSensor<std::int16_t> {
  public:
-  Temperature(const std::uint8_t mux_address,
+  Temperature(const std::uint8_t device_address,
               const std::uint8_t channel,
               io::I2c &i2c,
               hyped::core::ILogger &log);
   ~Temperature();
 
   core::Result configure();
-  std::optional<std::uint16_t> read();
+  std::optional<std::int16_t> read();
   std::uint8_t getChannel();
 
  private:
   hyped::core::ILogger &log_;
   hyped::io::I2c &i2c_;
-  const std::uint8_t mux_address_;
+  const std::uint8_t device_address_;
   const std::uint8_t channel_;
 };
 
