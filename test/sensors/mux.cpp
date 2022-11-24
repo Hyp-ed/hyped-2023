a656c639edec17339d3c7ece0fd34fc0a8543d41
Franz Miltz <franz@miltz.me>
2022-11-24 10:59:54 +0000

    Test mux and dummies

---
+++ test/sensors/mux.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include <core/logger.hpp>
+#include <sensors/mux.hpp>
+#include <utils/dummy_i2c.hpp>
+#include <utils/dummy_i2c_sensor.hpp>
+#include <utils/dummy_logger.hpp>
+
+namespace hyped::test {
+
+TEST(Mux, construction)
+{
+  static constexpr std::uint8_t kSize = 8;
+  std::array<std::unique_ptr<sensors::II2cMuxSensor<std::uint8_t>>, kSize> sensors;
+  for (auto &sensor : sensors) {
+    sensor = std::make_unique<utils::DummyI2cSensor>();
+  }
+  utils::DummyI2c i2c;
+  utils::DummyLogger logger;
+  sensors::Mux<std::uint8_t, kSize> mux(i2c, 0, sensors, logger);
+}
+
+}  // namespace hyped::test
