78a5655e6a482cb0de84207fd4edc7e78a93d199
Fayz-S <fayz.siddiqui@outlook.com>
2023-01-16 11:01:10 +0000

    Ran formatter

--- lib/navigation/preprocess_keyence.cpp
+++ lib/navigation/preprocess_keyence.cpp
@@ -20,8 +20,7 @@ SensorChecks KeyencePreprocessor::checkKeyenceAgrees(const core::KeyenceData &ke
 
   if (current_data_status == KeyenceDataStatus::kDisagreed
       && previous_data_status_ == KeyenceDataStatus::kDisagreed) {
-    log_.log(core::LogLevel::kFatal,
-             "Keyence disagreement for two consecutive readings.");
+    log_.log(core::LogLevel::kFatal, "Keyence disagreement for two consecutive readings.");
     return SensorChecks::kUnacceptable;
   }
   previous_data_status_ = current_data_status;
--- lib/navigation/preprocess_keyence.hpp
+++ lib/navigation/preprocess_keyence.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "consts.hpp"
 
 #include "core/logger.hpp"
