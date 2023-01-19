e750b889b63cf54a77cc962add3009730154921e
DylanCavers <s2179041@ed.ac.uk>
2023-01-19 17:39:49 +0000

    formatted

--- lib/state_machine/state_machine.hpp
+++ lib/state_machine/state_machine.hpp
@@ -20,23 +20,22 @@ class StateMachine {
   void reset();
 
   // TODO change transitions to actual transitions
-  std::array<Transition, kNumTransitions> transitions{{
-    {State::kIdle, State::kCalibrating, Message::mForward},
-    {State::kCalibrating, State::kReady, Message::mForward},
-    {State::kReady, State::kAccelerating, Message::mForward},
-    {State::kAccelerating, State::kCruising, Message::mForward},
-    {State::kCruising, State::kBraking, Message::mForward},
-    {State::kBraking, State::kStopped, Message::mForward},
-    {State::kStopped, State::kOff, Message::mForward},
-    {State::kIdle, State::kFailureStopped, Message::mFailure},
-    {State::kCalibrating, State::kFailureStopped, Message::mFailure},
-    {State::kReady, State::kFailureStopped, Message::mFailure},
-    {State::kAccelerating, State::kFailureBraking, Message::mFailure},
-    {State::kCruising, State::kFailureBraking, Message::mFailure},
-    {State::kBraking, State::kFailureBraking, Message::mFailure},
-    {State::kStopped, State::kFailureStopped, Message::mFailure},
-    {State::kFailureBraking, State::kFailureStopped, Message::mFailure}
-  }};
+  std::array<Transition, kNumTransitions> transitions{
+    {{State::kIdle, State::kCalibrating, Message::mForward},
+     {State::kCalibrating, State::kReady, Message::mForward},
+     {State::kReady, State::kAccelerating, Message::mForward},
+     {State::kAccelerating, State::kCruising, Message::mForward},
+     {State::kCruising, State::kBraking, Message::mForward},
+     {State::kBraking, State::kStopped, Message::mForward},
+     {State::kStopped, State::kOff, Message::mForward},
+     {State::kIdle, State::kFailureStopped, Message::mFailure},
+     {State::kCalibrating, State::kFailureStopped, Message::mFailure},
+     {State::kReady, State::kFailureStopped, Message::mFailure},
+     {State::kAccelerating, State::kFailureBraking, Message::mFailure},
+     {State::kCruising, State::kFailureBraking, Message::mFailure},
+     {State::kBraking, State::kFailureBraking, Message::mFailure},
+     {State::kStopped, State::kFailureStopped, Message::mFailure},
+     {State::kFailureBraking, State::kFailureStopped, Message::mFailure}}};
 
   State current_state;
 };
