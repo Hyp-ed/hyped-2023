a7252b07dfb29a08abfc936981a1299915c93d3e
SnickeyX <s2152674@ed.ac.uk>
2023-01-19 10:37:19 +0000

    naive construction test

---
+++ lib/utils/dummy_uart.hpp
@@ -0,0 +1,20 @@
+#include <io/uart.hpp>
+
+namespace hyped::utils {
+
+/**
+ * A basic implementation of Uart that does not require any hardware. This allow us to
+ *
+ * 1. demonstrate how to implement the IUart interface, and
+ * 2. allow for quick test implementations.
+ */
+class DummyUart : public io::IUart {
+ public:
+  DummyUart()  = default;
+  ~DummyUart() = default;
+
+  core::Result sendBytes(const char *tx, const std::uint8_t length);
+  core::Result readBytes(unsigned char *rx, const std::uint8_t length);
+};
+
+}  // namespace hyped::utils
