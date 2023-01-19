e011524cb50d551135e4923775e5b4297e68606b
BenET01 <s2055565@ed.ac.uk>
2023-01-19 19:22:21 +0000

    instantiated class baby

--- test/navigation/imu_preprocessing.cpp
+++ test/navigation/imu_preprocessing.cpp
@@ -1,8 +1,18 @@
 #include <gtest/gtest.h>
+#include <cmath>
 
 #include <core/types.hpp>
 #include <navigation/preprocess_imu.hpp>
+#include <core/logger.hpp>
+#include <utils/manual_time.hpp>
 
 namespace hyped::test {
+
+void test(){
+    utils::ManualTime manual_time;
+    core::Logger logger("test", core::LogLevel::kFatal, manual_time);
+    navigation::ImuPreprocessor imu_processer(logger);
+}
+
 
 }  // namespace hyped::test
