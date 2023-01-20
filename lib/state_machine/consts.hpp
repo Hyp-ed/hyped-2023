b4932156b8bfc638956bbc1ddb59115e014d2ddf
DylanCavers <s2179041@ed.ac.uk>
2023-01-20 11:52:34 +0000

    updated constants

--- lib/state_machine/consts.hpp
+++ lib/state_machine/consts.hpp
@@ -3,6 +3,6 @@
 namespace hyped::state_machine {
 
 static constexpr int kNumStates      = 16;
-static constexpr int kNumTransitions = 44;
+static constexpr int kNumTransitions = 45;
 
 }  // namespace hyped::state_machine
