d572ca47dc5898b76b57b81c0e465e49bda5dc32
aryansharma19 <aryansharma2119@gmail.com>
2023-01-23 19:44:59 +0000

    adding documentation

--- lib/navigation/preprocess_encoders.hpp
+++ lib/navigation/preprocess_encoders.hpp
@@ -14,19 +14,19 @@ class EncodersPreprocessor {
  public:
   EncodersPreprocessor(core::ILogger &logger);
 
-  /** TODO
-   * @brief
+  /** 
+   * @brief process encoder data to deal with outliers and unreliable sensors
    * 
    * @param encoder_data
-   * @return
+   * @return cleaned and reliable encoder data
   */
   std::optional<core::EncoderData> processData(const core::EncoderData encoder_data);
 
  private:
  
-  /** TODO
+  /** 
    * @brief tidies up the received data by detecting the outliers or data received from faulty 
-     sensors and replaces them with median value of the dataset.
+     sensors and replaces them with median value of the dataset
    *
    * @param encoder_data array containing the values received from various encoder sensors
    * @return consistent data with no outliers
@@ -34,21 +34,22 @@ class EncodersPreprocessor {
   std::optional<core::EncoderData> detectOutliers(const core::EncoderData encoder_data);
   
   /** TODO
-   * @brief
+   * @brief changes the value corresponding to the encoder sensor in the encoders_reliable_ array to false if the number of consecutive 
+   * outliers exceeds the threshold value
    * 
-   * @return
+   * @return SensorChecks::kUnacceptable if more than 1 sensors have become unreliable and SensorChecks::kAcceptable otherwise
   */
   SensorChecks checkReliable();
 
-  /** TODO
-   * @brief
+  /** 
+   * @brief calculates a specific quantile value from input array
    * 
-   * @param reliable_data
-   * @param quartile_percent
-   * @return 
+   * @param reliable_data array containing filtered values(i.e with no values from faulty sensors)
+   * @param quartile_percent value corresponding to the quantile that we want(0.25 for q1, 0.5 for median and 0.75 for q3)
+   * @return the quantile that we require
   */
   template<std::size_t N>
-  core::Float getSpecificQuartile(const std::array<std::uint32_t, N> &reliable_data, const core::Float quartile_percent){
+  core::Float getSpecificQuantile(const std::array<std::uint32_t, N> &reliable_data, const core::Float quartile_percent){
 
     const core::Float quartile_index = (num_reliable_encoders_ - 1)*quartile_percent;
     const std::uint8_t quartile_high = static_cast<std::uint8_t>(std::ceil(quartile_index));
@@ -57,18 +58,18 @@ class EncodersPreprocessor {
     return quartile;
 }
 
-/** TODO
- * @brief
+/** 
+ * @brief cacluates the quartiles (q1,median,q3)
  * 
- * @param reliable_data
- * @return
+ * @param array containing filtered values(i.e with no values from faulty sensors)
+ * @return a quartile object which contains q1, q3 and the median value
 */
  template<std::size_t N>
  Quartile getQuartiles(std::array<std::uint32_t, N> &reliable_data){
     std::sort(reliable_data.begin(), reliable_data.end());
-    return {.q1     = getSpecificQuartile(reliable_data, 0.25),
-            .median = getSpecificQuartile(reliable_data, 0.5),
-            .q3     = getSpecificQuartile(reliable_data, 0.75)};
+    return {.q1     = getSpecificQuantile(reliable_data, 0.25),
+            .median = getSpecificQuantile(reliable_data, 0.5),
+            .q3     = getSpecificQuantile(reliable_data, 0.75)};
 }
 
   // number of reliable encoders, initialised as core::kNumEncoders
@@ -80,10 +81,10 @@ class EncodersPreprocessor {
   // initialised as all true, bool mask of reliable sensors
   std::array<bool, core::kNumEncoders> encoders_reliable_;
 
-  //TODO: comment use
+  //number of allowed consecutive outliers from a single encoder
   std::uint8_t max_consecutive_outliers_ = 10;
 
-  //TODO: comment use
+  //for logging fail state
   core::ILogger &logger_;
 };
 
---
+++ test/navigation/preprocess_encoders.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <core/logger.hpp>
+#include <core/types.hpp>
+#include <navigation/preprocess_encoders.hpp>
+#include <utils/manual_time.hpp>
+
+namespace hyped::test {
+
+core::Float epsilon = 1e-5;
+
+bool checkArrayEquality(core::EncoderData &encoder_data_a, core::EncoderData &encoder_data_b){
+    if(encoder_data_a.size() == encoder_data_b.size()){
+        for(std::size_t i = 0; i < encoder_data_a.size();++i){
+            if(std::abs(encoder_data_a.at(i) - encoder_data_b.at(i) >= epsilon)){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+void test()
+{
+  utils::ManualTime manual_time;
+  core::Logger logger("test", core::LogLevel::kFatal, manual_time);
+  navigation::EncodersPreprocessor encoder_processer(logger);
+}
+
+TEST(Imu, equal_data)
+{
+  utils::ManualTime manual_time;
+  core::Logger logger("test", core::LogLevel::kFatal, manual_time);
+  navigation::EncodersPreprocessor encoder_processer(logger);
+  core::EncoderData data                          = {1};
+  core::EncoderData answer                           = {1};
+  auto final_data = encoder_processer.processData(data);
+  ASSERT_TRUE(checkArrayEquality(*final_data, answer));
+}
+
+TEST(Imu, not_equal_data)
+{
+  utils::ManualTime manual_time;
+  core::Logger logger("test", core::LogLevel::kFatal, manual_time);
+  navigation::EncodersPreprocessor encoder_processer(logger);
+  core::EncoderData data                   = {6, 1, 1, 1};
+  core::EncoderData answer                    = {1};
+  auto final_data = encoder_processer.processData(data);
+  ASSERT_TRUE(checkArrayEquality(*final_data, answer));
+}
+
+TEST(Imu, one_unreliable_sensor)
+{
+  utils::ManualTime manual_time;
+  core::Logger logger("test", core::LogLevel::kFatal, manual_time);
+  navigation::EncodersPreprocessor encoder_processer(logger);
+  core::EncoderData data = {6,1,1,1};
+  for (std::size_t i; i < 12; ++i) {
+    encoder_processer.processData(data);
+  }
+  core::EncoderData answer                    = {1};
+  auto final_data = encoder_processer.processData(data);
+  ASSERT_TRUE(checkArrayEquality(*final_data, answer));
+}
+}  // namespace hyped::test
