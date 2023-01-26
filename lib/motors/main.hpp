ee125e3ca8167a976420516ae748aadc002945bb
tom <tfeholland@gmail.com>
2023-01-26 18:43:44 +0000

    Added comments to staes for development @Trist

--- lib/motors/main.hpp
+++ lib/motors/main.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
 namespace hyped::motors {
-  // Temporary states, actual states TBC.
-enum class MotorStates { kInitialisation, kCalibration, kReady, kAcceleration, kNominalBreaking, kStopped, kFailureBreaking, kMotorBreaking, kFrictionBreaking, kMitigate, kPEAccelerating, kPECruising, kPEBreaking };
+// Temporary states, actual states TBC.
+enum class MotorStates {
+  kInitialisation,
+  kCalibration, // Probably nothing to be done in this state
+  kReady, // Probably nothing to be done in this state
+  kAcceleration, // Send motor to drive
+  kNominalBreaking, // Generic breaking state
+  kStopped, // Generic breaking state
+  kFailureBreaking, // Generic breaking state
+  kMotorBreaking, // non generic LIM breaking state
+  kFrictionBreaking, // Generic breaking state
+  kMitigate, // Probably nothing to be done in this state
+  kPEAccelerating,
+  kPECruising,
+  kPEBreaking // Generic breaking state
+};
 
 class Main {
  public:
