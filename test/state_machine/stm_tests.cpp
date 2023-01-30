88f368829e246c5d03bbeb54877b095df381da79
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2023-01-30 19:18:28 +0000

    first attempt

--- test/state_machine/stm_tests.cpp
+++ test/state_machine/stm_tests.cpp
@@ -1,5 +1,12 @@
 #include <gtest/gtest.h>
 
+#include <state_machine/state_machine.hpp>
+
 namespace hyped::test {
-// TODO transition testing
+
+TEST(Transition, construction) {
+    state_machine::StateMachine testSM;
+    testSM.handleMessage(state_machine::Message mForward);
+    assert(testSM.current_state == state_machine::State::kCalibrating);
+}
 }  // namespace hyped::test
