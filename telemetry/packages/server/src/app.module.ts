627c037d7131a843e2451751884038b3c78f88ac
Arjun Naha <arjunnaha@users.noreply.github.com>
2023-01-30 19:05:54 +0000

    feat(tel-server): Add basic InfluxDB

--- telemetry/packages/server/src/app.module.ts
+++ telemetry/packages/server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InfluxModule } from './modules/influx/Influx.module';
 
 @Module({
-  imports: [],
+  imports: [InfluxModule],
   controllers: [AppController],
   providers: [AppService],
 })
