33f1b2db5fd1bdb500f2290594e5b34afa72a65e
Tristan Crocker <tristcrocker@gmail.com>
2023-02-02 18:29:14 +0000

    processMessage funct

--- lib/motors/can_processor.cpp
+++ lib/motors/can_processor.cpp
@@ -1,5 +1,6 @@
 #include "can_processor.hpp"
 #include "controller.hpp"
+#include "intrin.h"
 
 namespace hyped::motors {
 
@@ -16,6 +17,20 @@ bool CanProcessor::sendMessage(const core::CanFrame frame)
 
 void CanProcessor::processMessage(const core::CanFrame frame)
 {
+  uint16_t = frame.data;
+
+  if (frame.can_id == 0x580) {
+    if (frame.data) {
+      controller_->processErrorMessage(frame.data);
+    }else {
+      controller_->processWarningMessage(frame.data);
+    }
+
+  }
+  else {
+
+  }
+  
 }
 
 }  // namespace hyped::motors
