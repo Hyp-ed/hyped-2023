2776a1dd73707af479ae9ead1554471618869d92
HTTYDKing <s2335450@ed.ac.uk>
2023-02-02 18:34:01 +0000

    Changes made to naming convension and fixes to the status check

--- lib/sensors/temperature.cpp
+++ lib/sensors/temperature.cpp
@@ -3,7 +3,7 @@
 namespace hyped::sensors {
 
 Temperature::Temperature(hyped::core::ILogger &log, io::I2c &i2c, const std::uint8_t channel)
-    : log_(log),
+    : logger_(logger),
       i2c_(i2c),
       channel_(channel)
 {
@@ -15,26 +15,49 @@ Temperature::~Temperature()
 
 std::optional<std::int16_t> Temperature::read()
 {
-  const auto status_check_result = i2c_.readByte(kTemperature, kStatus);
+  const auto status_check_result = i2c_.readByte(kTemperatureDefaultAddress, kStatus);
   if (!status_check_result) {
-    log_.log(hyped::core::LogLevel::kFatal, "Temperature could not read status");
+    logger_.log(hyped::core::LogLevel::kFatal,
+                "Failed to read temperature sensor status at channel %d",
+                channel_);
     return std::nullopt;
   }
-  if (status_check_result.value() == 0) {
-    log_.log(hyped::core::LogLevel::kFatal, "Temperature sensor is not ready to be read from");
+  // Status checks
+  if (status_check_result.value() == kBusy) {
+    logger_.log(hyped::core::LogLevel::kFatal,
+                "Failed to read, temperature is below lower limit at channel %d",
+                channel_);
     return std::nullopt;
   }
-  const auto temperature_high_byte = i2c_.readByte(kTemperature, kDataTH);
+  if (status_check_result.value() == kOverTemperatureHighLimit) {
+    logger_.log(hyped::core::LogLevel::kFatal,
+                "Failed to read, temperature is above higher limit at channel %d",
+                channel_);
+    return std::nullopt;
+  }
+  if (status_check_result.value() == kUnderTemperatureLowLimit) {
+    logger_.log(hyped::core::LogLevel::kFatal,
+                "Failed to read, temperature sensor is not ready to be read at channel %d",
+                channel_);
+    return std::nullopt;
+  }
+  // Reading the temperature and making checks if it retrieved the values
+  const auto temperature_high_byte = i2c_.readByte(kTemperatureDefaultAddress, kDataTH);
   if (!temperature_high_byte) {
-    log_.log(hyped::core::LogLevel::kFatal, "Temperature high could not be read");
+    logger_.log(
+      hyped::core::LogLevel::kFatal, "Failed to read temperature high at channel %d", channel_);
     return std::nullopt;
   }
-  const auto temperature_low_byte = i2c_.readByte(kTemperature, kDataTL);
+  const auto temperature_low_byte = i2c_.readByte(kTemperatureDefaultAddress, kDataTL);
   if (!temperature_low_byte) {
-    log_.log(hyped::core::LogLevel::kFatal, "Temperature low could not be read");
+    logger_.log(
+      hyped::core::LogLevel::kFatal, "Failed to read temperature low at channel %d", channel_);
     return std::nullopt;
   }
   const auto temperature = ((temperature_high_byte.value() << 8) | temperature_low_byte.value());
+
+  logger_.log(
+    hyped::core::LogLevel::kFatal, "Successfully read temperature sensor at channel %d", channel_);
   // Scaling temperature as per the datasheet
   return temperature * 0.01;
 }
@@ -42,11 +65,16 @@ std::optional<std::int16_t> Temperature::read()
 core::Result Temperature::configure()
 {
   const core::Result write_result
-    = i2c_.writeByteToRegister(kTemperature, kCtrl, kConfigurationSetting);
+    = i2c_.writeByteToRegister(kTemperatureDefaultAddress, kCtrl, kConfigurationSetting);
   if (write_result == hyped::core::Result::kFailure) {
-    log_.log(hyped::core::LogLevel::kFatal, "Temperature configure not implemented");
+    logger_.log(hyped::core::LogLevel::kFatal,
+                "Failed to configure temperature sensor at channel %d",
+                channel_);
     return core::Result::kFailure;
   }
+  logger_.log(hyped::core::LogLevel::kFatal,
+              "Successful to configure temperature sensor at channel %d",
+              channel_);
   return core::Result::kSuccess;
 }
 
--- lib/sensors/temperature.hpp
+++ lib/sensors/temperature.hpp
@@ -9,18 +9,22 @@
 #include <io/i2c.hpp>
 
 namespace hyped::sensors {
-
-static constexpr std::uint8_t kTemperature = 0x38;
-static constexpr std::uint8_t kCtrl        = 0x04;
-static constexpr std::uint8_t kDataTH      = 0x07;
-static constexpr std::uint8_t kDataTL      = 0x06;
-static constexpr std::uint8_t kStatus      = 0x05;
+// These numbers are taken from the data sheet
+static constexpr std::uint8_t kTemperatureDefaultAddress = 0x38;
+static constexpr std::uint8_t kCtrl                      = 0x04;
+static constexpr std::uint8_t kDataTemperatureHigh       = 0x07;
+static constexpr std::uint8_t kDataTemperatureLow        = 0x06;
+static constexpr std::uint8_t kStatus                    = 0x05;
+// The values to check the status of the temperature sensor from the 0x05 register
+static constexpr std::uint8_t kBusy                     = 0x01;
+static constexpr std::uint8_t kOverTemperatureHighLimit = 0x02;
+static constexpr std::uint8_t kUnderTemperatureLowLimit = 0x04;
 // Sets the sensor to continuous mode, sets IF_ADD_INC, and sets sampling rate to 200Hz
 static constexpr std::uint8_t kConfigurationSetting = 0x3c;
 
 class Temperature : public II2cMuxSensor<std::int16_t> {
  public:
-  Temperature(hyped::core::ILogger &log, io::I2c &i2c, const std::uint8_t channel);
+  Temperature(hyped::core::ILogger &logger, io::I2c &i2c, const std::uint8_t channel);
   ~Temperature();
 
   core::Result configure();
@@ -28,7 +32,7 @@ class Temperature : public II2cMuxSensor<std::int16_t> {
   std::uint8_t getChannel();
 
  private:
-  hyped::core::ILogger &log_;
+  hyped::core::ILogger &logger_;
   hyped::io::I2c &i2c_;
   const std::uint8_t channel_;
 };
