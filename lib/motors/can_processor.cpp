9640849a606f67ade1c5ded0299d24a42783558c
Tristan Crocker <tristcrocker@gmail.com>
2023-02-02 18:54:31 +0000

    Add little endian to big endian conversion
    Still not building

--- lib/motors/can_processor.cpp
+++ lib/motors/can_processor.cpp
@@ -1,8 +1,16 @@
 #include "can_processor.hpp"
 #include "controller.hpp"
-#include "intrin.h"
+#include <cstdint>
+#include <iostream>
+#include <climits>
+
+
 
 namespace hyped::motors {
+
+uint64_t little_endian_array_to_big_endian_int(const uint8_t little_endian_array[8]) {
+    return (little_endian_array[7]<<56|little_endian_array[6]<<48|little_endian_array[5]<<40|little_endian_array[4]<<32|little_endian_array[3] << 24) | (little_endian_array[2] << 16) | (little_endian_array[1] << 8) | little_endian_array[0];
+}
 
 CanProcessor::CanProcessor(std::shared_ptr<Controller> controller)
 {
@@ -11,19 +19,23 @@ CanProcessor::CanProcessor(std::shared_ptr<Controller> controller)
 
 bool CanProcessor::sendMessage(const core::CanFrame frame)
 {
+
   // TODOLater implement
   return false;
 }
 
+//Process little endian to big endian
+
+
 void CanProcessor::processMessage(const core::CanFrame frame)
 {
-  uint16_t = frame.data;
 
-  if (frame.can_id == 0x580) {
+
+  if (frame.can_id == 0x80) {
     if (frame.data) {
-      controller_->processErrorMessage(frame.data);
+      controller_->processErrorMessage(little_endian_array_to_big_endian_int(frame.data[0]));
     }else {
-      controller_->processWarningMessage(frame.data);
+      controller_->processWarningMessage(little_endian_array_to_big_endian_int(frame.data[1]));
     }
 
   }
