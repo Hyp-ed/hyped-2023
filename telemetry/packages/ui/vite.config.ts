b2fdfa850d5d80b3761a163490cff3608721181b
Arjun Naha <arjunnaha@users.noreply.github.com>
2023-02-04 15:32:51 +0000

    fix(tel-ui): Add main HTML index

--- telemetry/packages/ui/vite.config.ts
+++ telemetry/packages/ui/vite.config.ts
@@ -1,16 +1,18 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'app/index.html'),
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app/index.html'),
         openmct: resolve(__dirname, 'openmct/index.html'),
       },
       onwarn(warning, warn) {
+        // Can potentially be removed
         if (warning.code === 'EVAL') return;
         warn(warning);
       },
