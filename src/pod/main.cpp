d1883f7faa693e2776e638540e2f77a8f982ee85
Tom Lonergan <tomlonergan91@gmail.com>
2023-02-04 17:59:32 +0000

    testing does not build

--- src/pod/main.cpp
+++ src/pod/main.cpp
@@ -14,22 +14,20 @@ int main(int argc, char **argv)
   const auto execution_time = timer.measure_execution_time([time]() {
     hyped::core::Logger logger("CAN", hyped::core::LogLevel::kDebug, time);
     logger.log(hyped::core::LogLevel::kDebug, "CAN test");
-    // auto maybe_can = hyped::io::HardwareCan::create(logger, "can1");
-    // if (!maybe_can) {
-    //   logger.log(hyped::core::LogLevel::kFatal, "no can pain");
-    //   return;
-    // }
-    // logger.log(hyped::core::LogLevel::kDebug, "CAN socket enabled");
-    // auto can = *maybe_can;
-    // hyped::io::HardwareCan can = hyped::io::HardwareCan(logger);
-    // can.initialise("can1");
+    auto maybe_can = hyped::io::HardwareCan::create(logger, "can1");
+    if (!maybe_can) {
+      logger.log(hyped::core::LogLevel::kFatal, "no can pain");
+      return;
+    }
+    logger.log(hyped::core::LogLevel::kDebug, "CAN socket enabled");
+    auto can = *maybe_can;
     hyped::io::CanFrame frame;
     frame.can_id  = 1;
     frame.can_dlc = 8;
     for (int i = 0; i < 8; i++) {
       frame.data[i] = 5;
     }
-    // auto result = can.send(frame);
+    auto result = can.send(frame);
     if (result == hyped::core::Result::kFailure) {
       logger.log(hyped::core::LogLevel::kFatal, "no can send");
     } else {
