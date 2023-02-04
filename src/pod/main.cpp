e443f3cb0c5e3b8e1fb01a9a2ef4a1d4a824bb79
Tom Lonergan <tomlonergan91@gmail.com>
2023-02-04 17:56:31 +0000

    temp testing main

--- src/pod/main.cpp
+++ src/pod/main.cpp
@@ -4,6 +4,7 @@
 #include <core/timer.hpp>
 #include <core/types.hpp>
 #include <core/wall_clock.hpp>
+#include <io/hardware_can.hpp>
 #include <io/hardware_gpio.hpp>
 
 int main(int argc, char **argv)
@@ -11,19 +12,29 @@ int main(int argc, char **argv)
   hyped::core::WallClock time;
   hyped::core::Timer timer(time);
   const auto execution_time = timer.measure_execution_time([time]() {
-    hyped::core::Logger logger("GPIO", hyped::core::LogLevel::kDebug, time);
-    hyped::io::HardwareGpio gpio(logger);
-    auto gpio_reader_opt = gpio.getReader(0);
-    if (!gpio_reader_opt) {
-      logger.log(hyped::core::LogLevel::kFatal, "Error");
-      return;
+    hyped::core::Logger logger("CAN", hyped::core::LogLevel::kDebug, time);
+    logger.log(hyped::core::LogLevel::kDebug, "CAN test");
+    // auto maybe_can = hyped::io::HardwareCan::create(logger, "can1");
+    // if (!maybe_can) {
+    //   logger.log(hyped::core::LogLevel::kFatal, "no can pain");
+    //   return;
+    // }
+    // logger.log(hyped::core::LogLevel::kDebug, "CAN socket enabled");
+    // auto can = *maybe_can;
+    hyped::io::HardwareCan can = hyped::io::HardwareCan(logger);
+    can.initialise("can1");
+    hyped::io::CanFrame frame;
+    frame.can_id  = 1;
+    frame.can_dlc = 8;
+    for (int i = 0; i < 8; i++) {
+      frame.data[i] = 5;
     }
-    auto gpio_reader = *gpio_reader_opt;
-    if (gpio_reader->read() == hyped::core::DigitalSignal::kHigh) {
-      logger.log(hyped::core::LogLevel::kInfo, "High");
+    auto result = can.send(frame);
+    if (result == hyped::core::Result::kFailure) {
+      logger.log(hyped::core::LogLevel::kFatal, "no can send");
     } else {
-      logger.log(hyped::core::LogLevel::kInfo, "Low");
-    };
+      logger.log(hyped::core::LogLevel::kFatal, "yes can send");
+    }
   });
   std::cout << "Ran for " << execution_time.count() << " ns" << std::endl;
 }
