6dbd9640a741c3923dd50d8dac85d5a732cbbc2c
Arjun Naha <arjunnaha@users.noreply.github.com>
2023-02-06 17:07:56 +0000

    wip(ui): Consume dictionary endpoint

---
+++ telemetry/packages/ui/openmct/core/config.ts
@@ -0,0 +1,2 @@
+export const HTTP_DEBUG = process.env.HTTP_DEBUG === 'true'
+export const SERVER_ENDPOINT = process.env.SERVER_ENDPOINT || 'http://localhost:3000'
--- telemetry/packages/ui/openmct/plugins/dictionary-plugin.ts
+++ telemetry/packages/ui/openmct/plugins/dictionary-plugin.ts
@@ -1,4 +1,27 @@
 import { OpenMCT } from "openmct/dist/openmct"
+import { http } from "../core/http"
+import { ObjectIdentitifer } from "../types/ObjectIdentifier"
+
+function getDictionary() {
+  return http.get('/dictionary').json().then((data) => {
+    return data
+  })
+}
+
+const objectProvider = {
+  get: (identifier: ObjectIdentitifer) => {
+    return getDictionary().then((dictionary: any) => {
+      if (identifier.key === 'pod_1') {
+        return {
+          identifier,
+          name: dictionary.name,
+          type: 'folder',
+          location: 'ROOT'
+        }
+      }
+    })
+  }
+}
 
 export function DictionaryPlugin() {
   return function install(openmct: OpenMCT) {
