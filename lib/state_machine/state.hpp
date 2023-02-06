08ca58147d2a461b22a4316866a5c55d65973af6
DylanCavers <s2179041@ed.ac.uk>
2023-02-06 18:26:09 +0000

    removed mitigate features

--- lib/state_machine/state.hpp
+++ lib/state_machine/state.hpp
@@ -4,19 +4,14 @@ namespace hyped::state_machine {
 
 // State class containing all the states
 enum class State {
-  // TODO Add the actual states
   kIdle,
   kCalibrating,
   kReady,
-  kMitigate,
   kAccelerating,
-  kPEAccelerating,
   kCruising,
-  kPECruising,
   kNominalBraking,
   kMotorBraking,
   kFrictionBraking,
-  kPEBraking,
   kFailureBraking,
   kStopped,
   kFailureStopped,
