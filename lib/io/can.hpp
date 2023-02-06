231e60a66e071736482b482276a75964c3242fa0
Tom Lonergan <tomlonergan91@gmail.com>
2023-02-06 18:13:31 +0000

    unmoved constant

--- lib/io/can.hpp
+++ lib/io/can.hpp
@@ -3,8 +3,6 @@
 #include <core/types.hpp>
 
 namespace hyped::io {
-// every frame sent to the controller contains 8 bytes
-static constexpr std::uint8_t kControllerCanFrameLength = 8;
 
 class Can {
  public:
