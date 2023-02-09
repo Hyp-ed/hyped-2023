2cbee89a742666f300f706647b180e5630f50bd3
Arjun Naha <arjunnaha@users.noreply.github.com>
2023-02-09 19:09:45 +0000

    feat(tel-ui): Consume object types

--- telemetry/packages/ui/openmct/plugins/dictionary-plugin.ts
+++ telemetry/packages/ui/openmct/plugins/dictionary-plugin.ts
@@ -1,9 +1,16 @@
 import { OpenMCT } from "openmct/dist/openmct"
 import { http } from "../core/http"
 import { ObjectIdentitifer } from "../types/ObjectIdentifier"
+import { ObjectType } from "../types/objectType"
 
 function getDictionary() {
-  return http.get('/dictionary').json().then((data) => {
+  return http.get('openmct/dictionary').json().then((data) => {
+    return data
+  })
+}
+
+function getObjectTypes() {
+  return http.get('openmct/object-types').json<ObjectType[]>().then((data) => {
     return data
   })
 }
@@ -18,7 +25,37 @@ const objectProvider = {
           type: 'folder',
           location: 'ROOT'
         }
+      } else {
+        // TODO: Type :)
+        const measurement = dictionary.measurements.find((measurement: any) => measurement.key === identifier.key)
+
+        return {
+          identifier,
+          name: measurement.name,
+          // TODO: Maybe should check this exists
+          type: `hyped.${measurement.type}`,
+          telemetry: {
+            values: measurement.values
+          },
+          location: "hyped:pod_1"
+        }
       }
+    })
+  }
+}
+
+const compositionProvider = {
+  appliesTo: (domainObject: any) => {
+    return domainObject.identifier.namespace === 'hyped' && domainObject.type === 'folder'
+  },
+  load: (domainObject: any) => {
+    return getDictionary().then((dictionary: any) => {
+      return dictionary.measurements.map((measurement: any) => {
+        return {
+          namespace: 'hyped',
+          key: measurement.key
+        }
+      })
     })
   }
 }
@@ -30,6 +67,19 @@ export function DictionaryPlugin() {
       key: 'pod_1'
     }, openmct.priority.HIGH)
 
-    // openmct.types.
+    openmct.objects.addProvider('hyped', objectProvider)
+
+    getObjectTypes().then((objectTypes) => {
+      objectTypes.map((objectType) => {
+        openmct.types.addType(`hyped.${objectType.id}`, {
+          name: objectType.name,
+          // description: objectType.name,
+          cssClass: objectType.icon,
+        })
+      })
+    })
+
+    openmct.composition.addProvider(compositionProvider as any)
   }
 }
+
