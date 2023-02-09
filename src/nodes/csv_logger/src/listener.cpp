95023fb17be18b6d50a0c8c24768e63306d655c2
Kshitij Sharma <kshitij4sharma@gmail.com>
2023-02-09 15:25:57 +0000

    Missing STD and format

--- src/nodes/csv_logger/src/listener.cpp
+++ src/nodes/csv_logger/src/listener.cpp
@@ -1,36 +1,33 @@
-#include <memory>
 #include <fstream>
+#include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int16.hpp"
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
-{
-  public:
-    MinimalSubscriber()
-    : Node("csv_logger"),
-      count_(0)
-    {
-      subscription_ = this->create_subscription<std_msgs::msg::Int16>(
+class MinimalSubscriber : public rclcpp::Node {
+ public:
+  MinimalSubscriber() : Node("csv_logger"), count_(0)
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::Int16>(
       "random", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-      file_.open("data.csv");
-      file_ << "count, data" << std::endl;
-    }
+    file_.open("data.csv");
+    file_ << "count, data" << std::endl;
+  }
 
-  private:
-    void topic_callback(const std_msgs::msg::Int16::SharedPtr msg)
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->data);
-      file_ << count_ << ", " << msg->data << std::endl;
-      ++count_;
-    }
-    rclcpp::Subscription<std_msgs::msg::Int16>::SharedPtr subscription_;
-    std::ofstream file_;
-    uint16_t count_;
+ private:
+  void topic_callback(const std_msgs::msg::Int16::SharedPtr msg)
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->data);
+    file_ << count_ << ", " << msg->data << std::endl;
+    ++count_;
+  }
+  rclcpp::Subscription<std_msgs::msg::Int16>::SharedPtr subscription_;
+  std::ofstream file_;
+  std::uint16_t count_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
