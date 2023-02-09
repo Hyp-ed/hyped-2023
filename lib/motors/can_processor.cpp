45cec91c523739a08dfbbf7d937c5a9eb743b350
tom <tfeholland@gmail.com>
2023-02-09 18:41:02 +0000

    Not building but pushing for Trist to see

--- lib/motors/can_processor.cpp
+++ lib/motors/can_processor.cpp
@@ -1,15 +1,18 @@
 #include "can_processor.hpp"
 #include "controller.hpp"
+
+#include <climits>
 #include <cstdint>
 #include <iostream>
-#include <climits>
-
-
 
 namespace hyped::motors {
 
-uint64_t little_endian_array_to_big_endian_int(const uint8_t little_endian_array[8]) {
-    return (little_endian_array[7]<<56|little_endian_array[6]<<48|little_endian_array[5]<<40|little_endian_array[4]<<32|little_endian_array[3] << 24) | (little_endian_array[2] << 16) | (little_endian_array[1] << 8) | little_endian_array[0];
+uint64_t CanProcessor::little_endian_array_to_big_endian_int(const uint8_t little_endian_array[8])
+{
+  return (static_cast<uint64_t>(little_endian_array[7]) << 56 | static_cast<uint64_t>(little_endian_array[6]) << 48
+          | static_cast<uint64_t>(little_endian_array[5]) << 40 | static_cast<uint64_t>(little_endian_array[4]) << 32
+          | (static_cast<uint64_t>(little_endian_array[3]) << 24)
+         |(static_cast<uint64_t>(little_endian_array[2]) << 16) | (static_cast<uint64_t>(little_endian_array[1]) << 8) | static_cast<uint64_t>(little_endian_array[0]));
 }
 
 CanProcessor::CanProcessor(std::shared_ptr<Controller> controller)
@@ -19,30 +22,25 @@ CanProcessor::CanProcessor(std::shared_ptr<Controller> controller)
 
 bool CanProcessor::sendMessage(const core::CanFrame frame)
 {
-
   // TODOLater implement
   return false;
 }
 
-//Process little endian to big endian
-
+// Process little endian to big endian
 
 void CanProcessor::processMessage(const core::CanFrame frame)
 {
-
-
   if (frame.can_id == 0x80) {
+    // TODO: convert frame.data into index, subindex and data 
     if (frame.data) {
       controller_->processErrorMessage(little_endian_array_to_big_endian_int(frame.data[0]));
-    }else {
+    } else {
       controller_->processWarningMessage(little_endian_array_to_big_endian_int(frame.data[1]));
     }
 
+  } else {
+    // TODO: Implement
   }
-  else {
-
-  }
-  
 }
 
 }  // namespace hyped::motors
--- lib/motors/can_processor.hpp
+++ lib/motors/can_processor.hpp
@@ -9,9 +9,12 @@
 
 namespace hyped::motors {
 
+
+
 class CanProcessor {
  public:
   CanProcessor(std::shared_ptr<Controller> controller);
+  uint64_t little_endian_array_to_big_endian_int(const uint8_t little_endian_array[8]);
   bool sendMessage(const core::CanFrame frame);
   void processMessage(const core::CanFrame frame);
 
