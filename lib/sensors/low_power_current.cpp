7b841277e83d21f7e46e5eaa0fe62e18478517da
sa-fx <stewart.alexander2018@gmail.com>
2023-02-13 17:27:32 +0000

    Made PR amendments

--- lib/sensors/low_power_current.cpp
+++ lib/sensors/low_power_current.cpp
@@ -2,21 +2,12 @@
 
 namespace hyped::sensors {
 
-// TODO: Implement failure state for create
-std::optional<LowPowerCurrent> LowPowerCurrent::create(core::ILogger &logger,
-                                                       std::shared_ptr<io::II2c> i2c,
-                                                       const std::uint8_t channel
-                                                       = kDefaultI2cAddress)
-{
-  return LowPowerCurrent(logger, i2c, channel);
-}
-
 LowPowerCurrent::LowPowerCurrent(core::ILogger &logger,
                                  std::shared_ptr<io::II2c> i2c,
-                                 const std::uint8_t channel)
+                                 const std::uint8_t device_address)
     : logger_(logger),
-      channel_(channel),
-      i2c_(i2c)
+      i2c_(i2c),
+      device_address_(device_address)
 {
 }
 
@@ -26,19 +17,19 @@ LowPowerCurrent::~LowPowerCurrent()
 
 std::optional<core::Float> LowPowerCurrent::readCurrent()
 {
-  const auto byte = i2c_->readByte(channel_, kCurrentRegister);
+  const auto byte = i2c_->readByte(device_address_, kLowPowerCurrectRegister);
   if (!byte) {
-    logger_.log(core::LogLevel::kFatal, "Failed to read current on channel %d", channel_);
+    logger_.log(core::LogLevel::kFatal, "Failed to read current on channel %d", device_address_);
     return std::nullopt;
   }
-  logger_.log(core::LogLevel::kDebug, "Current read on channel %d successful", channel_);
+  logger_.log(core::LogLevel::kDebug, "Current read on channel %d successful", device_address_);
   // Divide milliAmps output by 1,000 to return Amps value
   return static_cast<core::Float>(*byte) / 1000;
 }
 
-std::uint8_t LowPowerCurrent::getChannel() const
+std::uint8_t LowPowerCurrent::getDeviceAddress() const
 {
-  return channel_;
+  return device_address_;
 }
 
 }  // namespace hyped::sensors
--- lib/sensors/low_power_current.hpp
+++ lib/sensors/low_power_current.hpp
@@ -10,31 +10,30 @@
 
 namespace hyped::sensors {
 
-// The datasheet lists four possible I2C addresses; 0x40 has been defined as the default
-constexpr std::uint8_t kDefaultI2cAddress = 0x40;
-constexpr std::uint8_t kI2cAddress2       = 0x41;
-constexpr std::uint8_t kI2cAddress3       = 0x44;
-constexpr std::uint8_t kI2cAddress4       = 0x45;
+// Four possible device addresses, as per the datasheet
+// 0x40 has been chosen as the default
+constexpr std::uint8_t kDefaultLowPowerCurrectAddress = 0x40;
+constexpr std::uint8_t kLowPowerCurrectAddress2       = 0x41;
+constexpr std::uint8_t kLowPowerCurrectAddress3       = 0x44;
+constexpr std::uint8_t kLowPowerCurrectAddress4       = 0x45;
 // Registers with reference to the INA219 driver code
-constexpr std::uint8_t kCurrentRegister = 0x04;
+constexpr std::uint8_t kLowPowerCurrectRegister = 0x04;
 
 class LowPowerCurrent {
  public:
-  static std::optional<LowPowerCurrent> create(core::ILogger &logger,
-                                               std::shared_ptr<io::II2c> i2c,
-                                               const std::uint8_t channel);
+  LowPowerCurrent(core::ILogger &logger,
+                  std::shared_ptr<io::II2c> i2c,
+                  const std::uint8_t device_address);
 
   ~LowPowerCurrent();
 
   std::optional<core::Float> readCurrent();
-  std::uint8_t getChannel() const;
-
- private:
-  LowPowerCurrent(core::ILogger &logger, std::shared_ptr<io::II2c> i2c, const std::uint8_t channel);
+  std::uint8_t getDeviceAddress() const;
 
  private:
   core::ILogger &logger_;
   std::shared_ptr<io::II2c> i2c_;
-  const std::uint8_t channel_;
+  const std::uint8_t device_address_;
 };
+
 }  // namespace hyped::sensors
