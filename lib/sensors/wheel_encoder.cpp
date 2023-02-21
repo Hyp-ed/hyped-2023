20ae2e020a56d081a0c5f89316afa9c1a3744c04
Vladimir Hanin <vladimir.hanin@outlook.comclear>
2023-02-21 20:30:57 +0000

    formatting

--- lib/sensors/wheel_encoder.cpp
+++ lib/sensors/wheel_encoder.cpp
@@ -2,13 +2,17 @@
 
 namespace hyped::sensors {
 
-std::optional<WheelEncoder> WheelEncoder::create(core::ILogger &logger, io::II2c &i2c, const std::uint8_t channel)
+std::optional<WheelEncoder> WheelEncoder::create(core::ILogger &logger,
+                                                 std::shared_ptr<io::II2c> i2c,
+                                                 const std::uint8_t channel)
 {
-  //resetWheelTurnCount();
+  // resetWheelTurnCount();
   return WheelEncoder(logger, i2c, channel);
 }
 
-WheelEncoder::WheelEncoder(core::ILogger &logger, io::II2c &i2c, const std::uint8_t channel)
+WheelEncoder::WheelEncoder(core::ILogger &logger,
+                           std::shared_ptr<io::II2c> i2c,
+                           const std::uint8_t channel)
     : logger_(logger),
       i2c_(i2c),
       channel_(channel)
@@ -22,19 +26,19 @@ WheelEncoder::~WheelEncoder()
 // TODOLater: do testing once we have the sensor !
 std::optional<std::uint32_t> WheelEncoder::getWheelTurnCount()
 {
-  const auto low_byte = i2c_.readByte(kDefaultWheelEncoderAddress, low_byte_address_);
+  const auto low_byte = i2c_->readByte(kDefaultWheelEncoderAddress, low_byte_address_);
   if (!low_byte) {
     logger_.log(core::LogLevel::kFatal, "Failed to read the low byte for wheel encoder");
     return std::nullopt;
   }
 
-  const auto middle_byte = i2c_.readByte(kDefaultWheelEncoderAddress, middle_byte_address_);
+  const auto middle_byte = i2c_->readByte(kDefaultWheelEncoderAddress, middle_byte_address_);
   if (!middle_byte) {
     logger_.log(core::LogLevel::kFatal, "Failed to read the middle byte for wheel encoder");
     return std::nullopt;
   }
 
-  const auto high_byte = i2c_.readByte(kDefaultWheelEncoderAddress, middle_byte_address_);
+  const auto high_byte = i2c_->readByte(kDefaultWheelEncoderAddress, middle_byte_address_);
   if (!high_byte) {
     logger_.log(core::LogLevel::kFatal, "Failed to read the high byte for wheel encoder");
     return std::nullopt;
@@ -45,7 +49,8 @@ std::optional<std::uint32_t> WheelEncoder::getWheelTurnCount()
 
 core::Result WheelEncoder::resetWheelTurnCount()
 {
-  const core::Result reset_result = i2c_.writeByteToRegister(kDefaultWheelEncoderAddress, kFreeRegisterAddress, kFreeRegisterValue);
+  const core::Result reset_result = i2c_->writeByteToRegister(
+    kDefaultWheelEncoderAddress, kFreeRegisterAddress, kFreeRegisterValue);
   if (reset_result == core::Result::kFailure) {
     logger_.log(core::LogLevel::kFatal, "Failed to reset count on the wheel encoder");
     return core::Result::kFailure;
--- lib/sensors/wheel_encoder.hpp
+++ lib/sensors/wheel_encoder.hpp
@@ -10,31 +10,31 @@
 namespace hyped::sensors {
 
 // ! these values still have to be found
-static constexpr std::uint8_t kDefaultWheelEncoderAddress = 0x0;
+constexpr std::uint8_t kDefaultWheelEncoderAddress = 0x0;
 
-static constexpr std::uint8_t low_byte_address_ = 0x0;
-static constexpr std::uint8_t middle_byte_address_ = 0x0;
-static constexpr std::uint8_t high_byte_address_ = 0x0;
+constexpr std::uint8_t low_byte_address_    = 0x0;
+constexpr std::uint8_t middle_byte_address_ = 0x0;
+constexpr std::uint8_t high_byte_address_   = 0x0;
 
-static constexpr std::uint8_t kFreeRegisterAddress = 0x0;
-static constexpr std::uint8_t kFreeRegisterValue = 0x2;
-
-
+constexpr std::uint8_t kFreeRegisterAddress = 0x0;
+constexpr std::uint8_t kFreeRegisterValue   = 0x2;
 
 class WheelEncoder {
  public:
-  static std::optional<WheelEncoder> create(core::ILogger &logger, io::II2c &i2c, const std::uint8_t channel);
+  static std::optional<WheelEncoder> create(core::ILogger &logger,
+                                            std::shared_ptr<io::II2c> i2c,
+                                            const std::uint8_t channel);
   ~WheelEncoder();
 
   std::optional<std::uint32_t> getWheelTurnCount();
   core::Result resetWheelTurnCount();
 
  private:
-  WheelEncoder(core::ILogger &logger, io::II2c &i2c, const std::uint8_t channel);
+  WheelEncoder(core::ILogger &logger, std::shared_ptr<io::II2c> i2c, const std::uint8_t channel);
 
  private:
   core::ILogger &logger_;
-  io::II2c &i2c_;
+  std::shared_ptr<io::II2c> i2c_;
   const std::uint8_t channel_;
 };
 
