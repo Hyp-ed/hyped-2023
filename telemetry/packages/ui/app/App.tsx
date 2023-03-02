3588e6ad5ff36d48f6edfd507209cd04d7ee5cfb
David Beechey <david@dtbeechey.dev>
2023-03-02 21:18:16 +0000

    feat(tel-ui): Add active braking toggle

--- telemetry/packages/ui/app/App.tsx
+++ telemetry/packages/ui/app/App.tsx
@@ -34,9 +34,17 @@ const App = () => {
     console.log(`Toggle active suspension: ${active ? 'active' : 'inactive'}`);
   };
 
+  /**
+   * Toggles active suspension
+   * @param active Whether active suspension is active
+   */
+  const toggleActiveBraking = (active: boolean) => {
+    console.log(`Toggle active braking: ${active ? 'active' : 'inactive'}`);
+  };
+
   return (
     <main
-      className={`px-4 py-8 flex flex-col justify-between h-full bg-[#393939] text-white select-none text-gray-100`}
+      className={`px-4 py-8 flex flex-col justify-between h-full bg-[#393939]select-none text-gray-100`}
     >
       <div className="flex flex-col justify-between h-full">
         <div className="space-y-2">
@@ -55,6 +63,7 @@ const App = () => {
               onChange={toggleActiveSuspension}
               text="Active Suspension"
             />
+            <Toggle onChange={toggleActiveBraking} text="Active Braking" />
           </div>
           <div className="flex flex-col gap-4">
             <Button onClick={go} colour="green" text="GO" />
