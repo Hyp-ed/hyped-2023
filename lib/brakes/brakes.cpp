578cd357200a67a9a413176e34496fc65159456a
root <root@LAPTOP-GQ4V9SLG.localdomain>
2023-03-06 18:45:42 +0000

    First Commit

---
+++ lib/brakes/brakes.cpp
@@ -0,0 +1,16 @@
+#include "brakes.hpp"
+
+brakes::Brakes(const std::uint8_t pin, const hyped::io::IGpio gpio) : pin_(pin)
+{
+  writer = gpio.getWriter(pin);
+}
+
+void brakes::stop()
+{
+  writer.write(core::DigitalSignal::kHigh);
+}
+
+void brakes::release()
+{
+  writer.write(core::DigitalSignal::kLow);
+}
---
+++ lib/brakes/brakes.hpp
@@ -0,0 +1,15 @@
+#include <core/types.hpp>
+#include <io/gpio.hpp>
+
+// This class deals with stopping and release brakes of the pod.
+
+class Brakes {
+ public:
+  Brakes(const std::uint8_t pin_, const hyped::io::IGpio gpio);
+  void stop();
+  void release();
+
+ private:
+  const std::uint8_t pin_;
+  hyped::io::IGpioWriter writer;
+}
