f1aa82003895dbfbc80a3eeb571c70194424a5d2
DylanCavers <s2179041@ed.ac.uk>
2023-03-06 20:32:34 +0000

    fixed publisher errors

--- src/ros/statemachine/src/publisher.cpp
+++ src/ros/statemachine/src/publisher.cpp
@@ -6,13 +6,10 @@
 
 class MinimalPublisher : public rclcpp::Node{
     public:
-    MinimalPublisher()
-        : Node("publisher"), 
-            count_(0)
-    
-    public:
-     MinimalPublisher()
-     : Node("minimal_publisher"), count_(0)
+     MinimalPublisher(hyped::core::ILogger &logger)
+     : Node("minimal_publisher"),
+       count_(0),
+       logger_(logger)
     {
         publisher_ = this ->create_publisher<std_msgs::msg::String>("topic", 10);
         timer_     = this->create_wall_timer(std::chrono::milliseconds(500),
@@ -24,13 +21,14 @@ class MinimalPublisher : public rclcpp::Node{
      {
         auto message = std_msgs::msg::String();
         message.data = "Hi! " + std::to_string(count_);
-        logger_.log(hyped::core::LogLevel::kInfo, "Publishing: '%d'", message.data);
-    publisher_->publish(message);
-    ++count_;
+        logger_.log(hyped::core::LogLevel::kInfo, "Publishing: '%s'", message.data.c_str());
+        publisher_->publish(message);
+        ++count_;
      }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    hyped::core::ILogger &logger_;
+     rclcpp::TimerBase::SharedPtr timer_;
+     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+     std::size_t count_;
+     hyped::core::ILogger &logger_;
 
 };
 
@@ -38,7 +36,7 @@ int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     hyped::core::WallClock time;
-    hyped::Logger logger("Topic", hyped::core::LogLevel::kDebug, time);
+    hyped::core::Logger logger("Topic", hyped::core::LogLevel::kDebug, time);
     rclcpp::spin(std::make_shared<MinimalPublisher>(logger));
     rclcpp::shutdown();
     return 0;
