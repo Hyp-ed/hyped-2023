637bd954410527a93c7e92180b084cfce8ee3fbf
Arjun Naha <arjunnaha@users.noreply.github.com>
2023-03-20 19:01:24 +0000

    feat(tel-server): Add keys to sensors master file

--- telemetry/packages/server/src/modules/openmct/dictionary/Dictionary.service.ts
+++ telemetry/packages/server/src/modules/openmct/dictionary/Dictionary.service.ts
@@ -10,39 +10,41 @@ export class DictionaryService {
       throw new Error(`Pod ${podId} not found`);
     }
 
-    const dictionary = pod.measurements.map((measurement) => {
-      return {
-        name: measurement.name,
-        key: measurement.key,
-        values: [
-          {
-            key: 'value',
-            name: 'Value',
-            units: measurement.units,
-            format: measurement.format,
-            ...(measurement.range && {
-              min: measurement.range?.min,
-              max: measurement.range?.max,
-            }),
-            ...(measurement.enumerations && {
-              enumerations: measurement.enumerations,
-            }),
-            hints: {
-              range: 1,
+    const dictionary = Object.entries(pod.measurements).map(
+      ([key, measurement]) => {
+        return {
+          name: measurement.name,
+          key: key,
+          values: [
+            {
+              key: 'value',
+              name: 'Value',
+              units: measurement.units,
+              format: measurement.format,
+              ...('range' in measurement && {
+                min: measurement.range?.min,
+                max: measurement.range?.max,
+              }),
+              ...('enumerations' in measurement && {
+                enumerations: measurement.enumerations,
+              }),
+              hints: {
+                range: 1,
+              },
             },
-          },
-          {
-            key: 'utc',
-            source: 'timestamp',
-            name: 'Timestamp',
-            format: 'utc',
-            units: {
-              domain: 'time',
+            {
+              key: 'utc',
+              source: 'timestamp',
+              name: 'Timestamp',
+              format: 'utc',
+              units: {
+                domain: 'time',
+              },
             },
-          },
-        ],
-      };
-    });
+          ],
+        };
+      },
+    );
 
     return {
       name: pod.name,
