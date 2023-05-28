9de728f5225082cd1c09d4bd2665174034285aa0
Tom Lonergan <tomlonergan91@gmail.com>
2023-05-28 20:27:06 +0100

    implement wheel encoder

--- lib/sensors/wheel_encoder.cpp
+++ lib/sensors/wheel_encoder.cpp
@@ -2,4 +2,28 @@
 
 namespace hyped::sensors {
 
+WheelEncoder::WheelEncoder(core::ILogger &logger, std::shared_ptr<io::IAdc> adc)
+    : logger_(logger),
+      adc_(adc),
+      count_(0)
+{
+}
+
+std::uint64_t WheelEncoder::getCount()
+{
+  return count_;
+}
+
+core::Result WheelEncoder::updateCount()
+{
+  const auto optional_voltage = adc_->readValue();
+  if (!optional_voltage) {
+    logger_.log(core::LogLevel::kFatal, "Failed to read wheel encoder value from ADC");
+    return core::Result::kFailure;
+  }
+  const core::Float voltage = *optional_voltage;
+  if (voltage > kVoltageThreshold) { count_++; }
+  return core::Result::kSuccess;
+}
+
 }  // namespace hyped::sensors
--- lib/sensors/wheel_encoder.hpp
+++ lib/sensors/wheel_encoder.hpp
@@ -7,18 +7,17 @@
 namespace hyped::sensors {
 class WheelEncoder {
  public:
-  std::optional<WheelEncoder> create(core::ILogger &logger, std::shared_ptr<io::IAdc> adc);
-
   std::uint64_t getCount();
 
-  void updateCount();
+  core::Result updateCount();
 
  private:
   WheelEncoder(core::ILogger &logger, std::shared_ptr<io::IAdc> adc);
 
  private:
+  static constexpr core::Float kVoltageThreshold = 1.7;
   core::ILogger &logger_;
   std::shared_ptr<io::IAdc> adc_;
-  const std::uint64_t count_;
+  std::uint64_t count_;
 };
 }  // namespace hyped::sensors
