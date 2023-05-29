7c2e7683a930ddb054cb7a3b8d5b3ffed590dddb
maxguy2001 <max.guy2001@gmail.com>
2023-05-29 11:41:49 +0100

    moving consts to central navigation file

--- lib/navigation/accelerometer_kalman.cpp
+++ lib/navigation/accelerometer_kalman.cpp
@@ -13,17 +13,13 @@ AccelerometerKalman::AccelerometerKalman(core::ILogger &logger,
 
       };
 
-Eigen::
-  Matrix<core::Float, AccelerometerKalman::state_dimension_, AccelerometerKalman::state_dimension_>
+Eigen::Matrix<core::Float, state_dimension, state_dimension>
   AccelerometerKalman::getStateTransitionMatrix(const core::Float time_delta)
 {
-  Eigen::Matrix<core::Float,
-                AccelerometerKalman::state_dimension_,
-                AccelerometerKalman::state_dimension_>
-    state_transition_matrix;
+  Eigen::Matrix<core::Float, state_dimension, state_dimension> state_transition_matrix;
   std::uint32_t factorial;
-  for (std::size_t i = 0; i < AccelerometerKalman::state_dimension_; i++) {
-    for (std::size_t j = 0; j < AccelerometerKalman::state_dimension_; j++) {
+  for (std::size_t i = 0; i < state_dimension; i++) {
+    for (std::size_t j = 0; j < state_dimension; j++) {
       factorial = 1;
       for (std::size_t k = 1; k <= (i); i++) {
         factorial *= k;
@@ -39,37 +35,29 @@ Eigen::
   return state_transition_matrix;
 }
 
-Eigen::Matrix<core::Float, AccelerometerKalman::state_dimension_, 1>
-  AccelerometerKalman::getMeasurementVector(core::Float acceleration)
+Eigen::Matrix<core::Float, state_dimension, 1> AccelerometerKalman::getMeasurementVector(
+  core::Float acceleration)
 {
-  Eigen::Matrix<core::Float, AccelerometerKalman::state_dimension_, 1> measurement_vector
-    = {acceleration, 0, 0};
+  Eigen::Matrix<core::Float, state_dimension, 1> measurement_vector = {acceleration, 0, 0};
   return measurement_vector;
 }
 
-Eigen::
-  Matrix<core::Float, AccelerometerKalman::state_dimension_, AccelerometerKalman::state_dimension_>
+// TODO: fix this!
+Eigen::Matrix<core::Float, state_dimension, state_dimension>
   AccelerometerKalman::getStateTransitionCovarianceMatrix(
-    Eigen::Matrix<core::Float, state_dimension_, state_dimension_> prior_state,
-    Eigen::Matrix<core::Float, state_dimension_, state_dimension_> prior_propagation)
+    Eigen::Matrix<core::Float, state_dimension, state_dimension> prior_state,
+    Eigen::Matrix<core::Float, state_dimension, state_dimension> prior_propagation)
 {
   // TODO: implement
-  Eigen::Matrix<core::Float,
-                AccelerometerKalman::state_dimension_,
-                AccelerometerKalman::state_dimension_>
-    state_transition_covariance_matrix;
+  Eigen::Matrix<core::Float, state_dimension, state_dimension> state_transition_covariance_matrix;
   return state_transition_covariance_matrix;
 }
 
-Eigen::Matrix<core::Float,
-              AccelerometerKalman::measurement_dimension_,
-              AccelerometerKalman::measurement_dimension_>
+Eigen::Matrix<core::Float, measurement_dimension, measurement_dimension>
   AccelerometerKalman::getMeasurementNoiseCovarianceMatrix()
 {
   // TODO: generate random noise then fix
-  Eigen::Matrix<core::Float,
-                AccelerometerKalman::measurement_dimension_,
-                AccelerometerKalman::measurement_dimension_>
+  Eigen::Matrix<core::Float, measurement_dimension, measurement_dimension>
     measurement_noise_covariance_matrix{0};
   return measurement_noise_covariance_matrix;
 }
@@ -81,24 +69,19 @@ core::Float AccelerometerKalman::filter()
   return 0.0F;
 }
 
-Eigen::Matrix<core::Float,
-              AccelerometerKalman::extended_dimension_,
-              AccelerometerKalman::state_dimension_>
+Eigen::Matrix<core::Float, extended_dimension, state_dimension>
   AccelerometerKalman::getJacobianMatrix(const core::Float time_delta)
 {
-  Eigen::Matrix<core::Float,
-                AccelerometerKalman::extended_dimension_,
-                AccelerometerKalman::state_dimension_>
-    jacobian_matrix;
+  Eigen::Matrix<core::Float, extended_dimension, state_dimension> jacobian_matrix;
   std::uint32_t factorial;
-  for (std::size_t i = 0; i < AccelerometerKalman::state_dimension_; i++) {
-    for (std::size_t j = 0; j < AccelerometerKalman::state_dimension_; j++) {
+  for (std::size_t i = 0; i < state_dimension; i++) {
+    for (std::size_t j = 0; j < state_dimension; j++) {
       factorial = 1;
-      for (std::size_t k = 1; i <= (i + state_dimension_); i++) {
+      for (std::size_t k = 1; i <= (i + state_dimension); i++) {
         factorial *= k;
       }
       jacobian_matrix(i, j)
-        = std::pow(time_delta, (i + state_dimension_)) / static_cast<core::Float>(factorial);
+        = std::pow(time_delta, (i + state_dimension)) / static_cast<core::Float>(factorial);
     }
   }
 
@@ -106,13 +89,11 @@ Eigen::Matrix<core::Float,
 }
 
 // TODO: find out how to calculate jerk and higher order derivatives of acceleration for this vector
-Eigen::Matrix<core::Float, AccelerometerKalman::extended_dimension_, 1>
-  AccelerometerKalman::getExtendedStateVector()
+Eigen::Matrix<core::Float, extended_dimension, 1> AccelerometerKalman::getExtendedStateVector()
 {
   // TODO: make call to separate function to calculate jerk and higher order derivatives of
   // acceleration
-  Eigen::Matrix<core::Float, AccelerometerKalman::extended_dimension_, 1> extended_state_vector{0,
-                                                                                                0};
+  Eigen::Matrix<core::Float, extended_dimension, 1> extended_state_vector{0, 0};
 
   return extended_state_vector;
 }
--- lib/navigation/accelerometer_kalman.hpp
+++ lib/navigation/accelerometer_kalman.hpp
@@ -28,9 +28,9 @@ class AccelerometerKalman {
 
   core::Float filter();
 
-  static constexpr std::size_t state_dimension_       = 3;  // TODO: change this!
-  static constexpr std::size_t measurement_dimension_ = 1;
-  static constexpr std::size_t extended_dimension_    = 2;  // TODO: check this!
+  // static constexpr std::size_t state_dimension_      = 3;  // TODO: change this!
+  // static constexpr std::size_t measurement_dimension_ = 1;
+  // static constexpr std::size_t extended_dimension_    = 2;  // TODO: check this!
 
  private:
   ExtendedKalmanFilter<3, 1, 2> kalman_filter_;
@@ -38,32 +38,28 @@ class AccelerometerKalman {
   core::ILogger &logger_;
   const core::ITimeSource &time_;
   // TODO: change for actual state vector
-  Eigen::Matrix<core::Float, state_dimension_, 1> initial_state = {0, 0, 0};
-  Eigen::Matrix<core::Float, state_dimension_, state_dimension_> initial_error_covariance;
+  Eigen::Matrix<core::Float, state_dimension, 1> initial_state = {0, 0, 0};
+  Eigen::Matrix<core::Float, state_dimension, state_dimension> initial_error_covariance;
   // TODO: make this less bad
   //{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-  ExtendedKalmanFilter<state_dimension_, measurement_dimension_, extended_dimension_>
-    kalman_filter_(
-      const core::ITimeSource &time_,
-      Eigen::Matrix<core::Float, state_dimension_, 1> initial_state,
-      Eigen::Matrix<core::Float, state_dimension_, state_dimension_> initial_error_covariance);
+  ExtendedKalmanFilter<state_dimension, measurement_dimension, extended_dimension> kalman_filter_(
+    const core::ITimeSource &time_,
+    Eigen::Matrix<core::Float, state_dimension, 1> initial_state,
+    Eigen::Matrix<core::Float, state_dimension, state_dimension> initial_error_covariance);
 
-  const Eigen::Matrix<core::Float, state_dimension_, state_dimension_> measurement_matrix_;
+  const Eigen::Matrix<core::Float, state_dimension, state_dimension> measurement_matrix_;
   //{{1, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
   // TODO: implement and document these functions
-  Eigen::Matrix<core::Float, state_dimension_, 1> getMeasurementVector(core::Float acceleration);
-  Eigen::Matrix<core::Float, state_dimension_, state_dimension_> getStateTransitionMatrix(
+  Eigen::Matrix<core::Float, state_dimension, 1> getMeasurementVector(core::Float acceleration);
+  Eigen::Matrix<core::Float, state_dimension, state_dimension> getStateTransitionMatrix(
     const core::Float time_delta);
-  Eigen::Matrix<core::Float, state_dimension_, state_dimension_>
-    getStateTransitionCovarianceMatrix();
-  Eigen::Matrix<core::Float,
-                AccelerometerKalman::measurement_dimension_,
-                AccelerometerKalman::measurement_dimension_>
+  Eigen::Matrix<core::Float, state_dimension, state_dimension> getStateTransitionCovarianceMatrix();
+  Eigen::Matrix<core::Float, measurement_dimension, measurement_dimension>
     getMeasurementNoiseCovarianceMatrix();
-  Eigen::Matrix<core::Float, extended_dimension_, state_dimension_> getJacobianMatrix(
+  Eigen::Matrix<core::Float, extended_dimension, state_dimension> getJacobianMatrix(
     const core::Float time_delta);
-  Eigen::Matrix<core::Float, extended_dimension_, 1> getExtendedStateVector();
+  Eigen::Matrix<core::Float, extended_dimension, 1> getExtendedStateVector();
 };
 
 }  // namespace hyped::navigation
