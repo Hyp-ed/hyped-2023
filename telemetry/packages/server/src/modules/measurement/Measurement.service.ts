9aa08b707e5a4a6d54d5f93ea10b4f4ef9a1385c
Arjun Naha <arjunnaha@users.noreply.github.com>
2023-06-01 23:45:24 +0100

    feat(tel-server): Add measurement service

---
+++ telemetry/packages/server/src/modules/measurement/Measurement.service.ts
@@ -0,0 +1,99 @@
+import { pods } from '@hyped/telemetry-constants';
+import { Point } from '@influxdata/influxdb-client';
+import { Injectable, LoggerService } from '@nestjs/common';
+import { z } from 'zod';
+import { InfluxService } from '../influx/Influx.service';
+import { Logger } from '../logger/Logger.decorator';
+
+const BaseMeasurementReading = z.object({
+  podId: z.string(),
+  measurementKey: z.string(),
+  value: z.number(),
+});
+
+type MeasurementReading = z.infer<typeof BaseMeasurementReading>;
+
+@Injectable()
+export class MeasurementService {
+  constructor(
+    @Logger()
+    private readonly logger: LoggerService,
+    private influxService: InfluxService,
+  ) {}
+
+  public addMeasurement(props: MeasurementReading) {
+    const currentTime = new Date();
+
+    const validatedMeasurement = this.validateMeasurementReading(props);
+
+    if (!validatedMeasurement) {
+      // do something more here...maybe
+      return;
+    }
+
+    const {
+      measurement,
+      reading: { podId, measurementKey, value },
+    } = validatedMeasurement;
+
+    const point = new Point('measurement')
+      .timestamp(currentTime)
+      .tag('podId', podId)
+      .tag('measurementKey', measurementKey)
+      .tag('format', measurement.format)
+      .floatField('value', value);
+
+    this.influxService.write.writePoint(point);
+
+    this.logger.verbose(
+      `Added measurement {${props.podId}/${props.measurementKey}}: ${props.value}`,
+      MeasurementService.name,
+    );
+  }
+
+  private logValidationError(message: string, props: MeasurementReading) {
+    this.logger.error(
+      `${message} {${props.podId ?? 'unknownPod'}/${
+        props.measurementKey ?? 'unknownMeasurement'
+      }}: ${props.value ?? 'no value'}`,
+      null,
+      MeasurementService.name,
+    );
+  }
+
+  private validateMeasurementReading(props: MeasurementReading) {
+    const result = BaseMeasurementReading.safeParse(props);
+    if (!result.success) {
+      this.logValidationError('Invalid measurement reading', props);
+      return;
+    }
+
+    const { podId, measurementKey, value } = result.data;
+
+    const pod = pods[podId];
+    if (!pod) {
+      this.logValidationError('Pod not found', props);
+      return;
+    }
+
+    const measurement = pods[podId]['measurements'][measurementKey];
+    if (!measurement) {
+      this.logValidationError('Measurement not found', props);
+      return;
+    }
+
+    if (measurement.format === 'enum') {
+      const enumValue = measurement.enumerations.find((e) => e.value === value);
+
+      if (!enumValue) {
+        this.logValidationError('Invalid enum value', props);
+        return;
+      }
+    }
+
+    return {
+      reading: result.data,
+      measurement,
+    };
+  }
+}
--- telemetry/packages/server/src/modules/mqtt/ingestion/MqttIngestion.service.ts
+++ telemetry/packages/server/src/modules/mqtt/ingestion/MqttIngestion.service.ts
@@ -1,59 +1,22 @@
-import { pods, POD_IDS } from '@hyped/telemetry-constants';
 import { Injectable, LoggerService } from '@nestjs/common';
-import { Params, Payload, Subscribe, Topic } from 'nest-mqtt';
+import { Params, Payload, Subscribe } from 'nest-mqtt';
 import { Logger } from 'src/modules/logger/Logger.decorator';
-import { z } from 'zod';
-
-const MqttMessage = z.object({
-  podId: z.enum(POD_IDS),
-  measurementName: z.string(),
-  value: z.coerce.string(),
-});
+import { MeasurementService } from 'src/modules/measurement/Measurement.service';
 
 @Injectable()
 export class MqttIngestionService {
   constructor(
     @Logger()
     private readonly logger: LoggerService,
+    private measurementService: MeasurementService,
   ) {}
 
   @Subscribe('hyped/+/+')
-  getNotifications(
-    @Topic() rawTopic: string,
-    @Params() rawParams: string[],
-    @Payload() rawValue: any,
-  ) {
-    const result = MqttMessage.safeParse({
-      podId: rawParams[0],
-      measurementName: rawParams[1],
-      value: rawValue,
-    });
+  getNotifications(@Params() rawParams: string[], @Payload() rawValue: any) {
+    const podId = rawParams[0];
+    const measurementKey = rawParams[1];
+    const value = rawValue;
 
-    if (!result.success) {
-      this.logger.error(
-        `Invalid MQTT message {${rawTopic}}: ${result.error.message}`,
-        null,
-        MqttIngestionService.name,
-      );
-      return;
-    }
-
-    const { podId, measurementName, value } = result.data;
-
-    const measurement = pods[podId]['measurements'][measurementName];
-
-    if (!measurement) {
-      this.logger.error(
-        `Invalid MQTT message {${rawTopic}}: Measurement not found`,
-        null,
-        MqttIngestionService.name,
-      );
-      return;
-    }
-
-    this.logger.debug(
-      `Incoming measurement reading {${measurement.name}}: ${value}}`,
-      MqttIngestionService.name,
-    );
+    this.measurementService.addMeasurement({ podId, measurementKey, value });
   }
 }
