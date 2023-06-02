d8451e8212ee81ff2aa13266a257efed913a6c21
Tom Lonergan <tomlonergan91@gmail.com>
2023-06-02 13:35:15 +0100

    thermistor

--- lib/sensors/thermistor.hpp
+++ lib/sensors/thermistor.hpp
@@ -6,22 +6,26 @@
 
 #include <core/logger.hpp>
 #include <io/adc.hpp>
+#include <mux_sensors.hpp>
 
 namespace hyped::sensors {
 
 // Skeleton code for the thermistor class
 // TODOLater: Implement this properly
-class Thermistor {
+class Thermistor : public IMuxSensor<core::Float> {
  public:
-  static std::optional<Thermistor> create(core::ILogger &logger, std::shared_ptr<io::IAdc> adc);
+  static std::optional<Thermistor> create(core::ILogger &logger,
+                                          std::shared_ptr<io::IAdc> adc,
+                                          std::uint8_t channel);
+  Thermistor(core::ILogger &logger, std::shared_ptr<io::IAdc> adc, std::uint8_t channel);
   ~Thermistor();
-
- private:
-  Thermistor(core::ILogger &logger, std::shared_ptr<io::IAdc> adc);
+  std::uint8_t getChannel() const override;
+  std::optional<core::Float> read() override;
 
  private:
   core::ILogger &logger_;
   std::shared_ptr<io::IAdc> adc_;
+  std::uint8_t channel_;
 };
 
 }  // namespace hyped::sensors
