cebf66b6559efd391f98e06ac61763509ca278d4
David Beechey <david@dtbeechey.dev>
2023-07-18 21:54:49 +0100

    Kshitij MacBook changes 2

--- telemetry/mqtt/tcp_to_mqtt.py
+++ telemetry/mqtt/tcp_to_mqtt.py
@@ -4,6 +4,7 @@ import json
 
 # create a socket to receive data from the TCP sender
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # get local machine name
 HOST = "127.0.0.1"
@@ -17,6 +18,7 @@ mqtt_client.connect("localhost", 1883, 60)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    # Wait for multiple connections
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
@@ -26,5 +28,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
             # loop through the dictionary and publish each key-value pair
             for key, value in json.loads(data).items():
-                mqtt_client.publish(f'hyped/pod_1/measurement/{key}', value)
+                if key.startswith("accelerometer"):
+                    mqtt_client.publish(
+                        "hyped/pod_1/measurement/" + key, value["x"])
+                else:
+                    mqtt_client.publish(
+                        f'hyped/pod_1/measurement/{key}', value)
                 print(f"Published {key}: {value}")
