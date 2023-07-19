f2807076939a9de818dcc3b30399557fe6474cc7
maxguy2001 <max.guy2001@gmail.com>
2023-07-19 10:10:08 +0100

    fixed breaking distances

--- lib/navigation/control/consts.hpp
+++ lib/navigation/control/consts.hpp
@@ -37,7 +37,8 @@ using JacobianMatrix
 
 // TODOLater: move most of these to a config file
 static constexpr core::Float kTrackLength       = 100.0;  // m
-static constexpr core::Float kBrakingDistance   = 20.0;   // m TODOLater:check!
+static constexpr core::Float kBrakingDistance   = 45.0;   // m TODOLater:check!
+static constexpr core::Float kBreakingBuffer    = 10.0;   // m
 static constexpr core::Float kPi                = 3.14159265359;
 static constexpr core::Float kWheelCicumference = kPi * 0.1;  // m TODOLater: check!
 static constexpr core::Float kStripeDistance    = 5.0;
