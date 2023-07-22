db2a68ab96db6b5df26729dbc9db2de675f49a64
Arjun Naha <arjunnaha@users.noreply.github.com>
2023-07-22 03:29:35 +0100

    fix(tel-ui): Remove commented out code

--- telemetry/packages/ui/openmct/plugins/faults-plugin/faults-plugin.ts
+++ telemetry/packages/ui/openmct/plugins/faults-plugin/faults-plugin.ts
@@ -14,20 +14,6 @@ export function FaultsPlugin() {
       supportsSubscribe: realtimeProvider.supportsSubscribe,
       subscribe: realtimeProvider.subscribe,
       request: historicalProvider.request,
-      // acknowledgeFault(fault: Fault, { comment = '' }) {
-      //   utils.acknowledgeFault(fault);
-
-      //   return Promise.resolve({
-      //     success: true
-      //   });
-      // },
-      // shelveFault(fault: Fault, duration: any) {
-      //   utils.shelveFault(fault, duration);
-
-      //   return Promise.resolve({
-      //     success: true
-      //   });
-      // },
     });
   };
 }
